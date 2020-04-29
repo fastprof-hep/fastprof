7752ddb10e8b3deed431977cfb99684843a81f69
Nicolas Berger <nicolas.berger@cern.ch>
2020-04-29 16:38:30 +0200

    update script using new utilities

--- examples/sampling_limits_simple.py
+++ examples/sampling_limits_simple.py
@@ -1,6 +1,6 @@
 # For now this is a script -- should be upgraded to a generally usable class when the process is mature
 
-from fastprof import Model, Data, QMu, Samples, CLsSamples, OptiSampler, OptiMinimizer
+from fastprof import Model, Data, QMu, Samples, CLsSamples, OptiSampler, OptiMinimizer, FitResults
 import json
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,47 +20,41 @@ import scipy
 
 model_filename = 'run/high_mass_gg_1700-100bins.json'
 data_filename = 'run/high_mass_gg_1700-100bins-data.json'
-hypos_filename = 'run/hypos_high_mass_gg_1700.json'
-poi = 'mu'
-poi_init = 1
-poi_min = 0
-poi_max = 20
+fits_filename = 'run/fits_high_mass_gg_1700.json'
 output_filename = 'samples/high_mass_gg_1700-100bins'
 ntoys = 10000
 
-with open(hypos_filename, 'r') as fd :
-  hypo_dicts = json.load(fd)
-
-hypo_mus = [ hd[poi] for hd in hypo_dicts ]
+full_results = FitResults(fits_filename)
+fit_results = full_results.fit_results
 
 fast_model = Model.create(model_filename)
 
 np.random.seed(131071)
+fr = full_results
 opti_samples = CLsSamples(
-  Samples(OptiSampler(fast_model, mu0 = poi_init, bounds=(poi_min, poi_max))               , output_filename),
-  Samples(OptiSampler(fast_model, mu0 = poi_init, bounds=(poi_min, poi_max), do_CLb = True), output_filename + '_clb')).generate_and_save(hypo_mus, ntoys)
+  Samples(OptiSampler(fast_model, mu0=fr.poi_initial_value, bounds=(fr.poi_min, fr.poi_max))               , output_filename),
+  Samples(OptiSampler(fast_model, mu0=fr.poi_initial_value, bounds=(fr.poi_min, fr.poi_max), do_CLb = True), output_filename + '_clb')).generate_and_save(fr.hypos, ntoys)
 
-for hd in hypo_dicts :
-  if not 'cl' in hd :
-    hd['cl'] = QMu(hd['qmu'], hd[poi], hd['best_fit_val']).asymptotic_cl()
-  if not 'cls' in hd :
-    hd['cls'] = QMu(hd['qmu'], hd[poi], hd['best_fit_val']).asymptotic_cls(hd['best_fit_err'])
-  hd['sampling_cl'] = opti_samples.clsb.cl(hd['cl'], hd[poi])
-  hd['sampling_cls'] = opti_samples.cl(hd['cl'], hd[poi])
+full_results.fill() # fill asymptotic results
+for fit_result in fit_results :
+  fit_result['sampling_cl'] = opti_samples.clsb.cl(fit_result['cl'], fit_result[fr.poi_name])
+  fit_result['sampling_cls'] = opti_samples.cl(fit_result['cl'], fit_result[fr.poi_name])
 
 # Check the fastprof CLs against the ones in the reference: in principle this should match well,
 # otherwise it means what we generate isn't exactly comparable to the observation, which would be a problem...
+print('Check CL computed from fast model against those of the full model (a large difference would require to correct the sampling distributions) :')
 fast_data = Data(fast_model).load(data_filename)
-
-for hd in hypo_dicts :
-  tmu, min_mu = OptiMinimizer(fast_data, hd[poi], (-3, 20)).tmu(hd[poi])
-  q = QMu(tmu, hd[poi], min_mu)
-  print('mu = %g : observed asymptotic reference CL = %6.4f , fast CL = %6.4f (a large difference would require to correc the sampling distributions)' % (hd[poi], hd['cl'], q.asymptotic_cl()))
+full_results.check(fast_data)
 
 # Plot
 def find_hypo(mus, cls, cl = 0.05, n = 0) :
   logcls = [ math.log(c/cl) if c > 0 else -999 for c in cls[n:] ]
   finder = scipy.interpolate.InterpolatedUnivariateSpline(mus[n:], logcls, k=3)
+  if len(finder.roots()) == 0 :
+    print('No solution found')
+    return None
+  if len(finder.roots()) > 1 :
+    print('Multiple solutions found, returning the first one')
   return finder.roots()[0]
 
 plt.ion()
@@ -69,21 +63,21 @@ plt.figure(1)
 plt.suptitle('$CL_{s+b}$')
 plt.xlabel('$\mu$')
 plt.ylabel('$CL_{s+b}$')
-plt.plot(hypo_mus, [ hd['cl']          for hd in hypo_dicts ], 'r:' , label = 'Asymptotics')
-plt.plot(hypo_mus, [ hd['sampling_cl'] for hd in hypo_dicts ], 'b'  , label = 'Sampling')
+plt.plot(fr.hypos, [ fit_result['cl']          for fit_result in fit_results ], 'r:' , label = 'Asymptotics')
+plt.plot(fr.hypos, [ fit_result['sampling_cl'] for fit_result in fit_results ], 'b'  , label = 'Sampling')
 plt.legend()
 
 plt.figure(2)
 plt.suptitle('$CL_s$')
 plt.xlabel('$\mu$')
 plt.ylabel('$CL_s$')
-plt.plot(hypo_mus, [ hd['cls']          for hd in hypo_dicts ], 'r:' , label = 'Asymptotics')
-plt.plot(hypo_mus, [ hd['sampling_cls'] for hd in hypo_dicts ], 'b'  , label = 'Sampling')
+plt.plot(fr.hypos, [ fit_result['cls']          for fit_result in fit_results ], 'r:' , label = 'Asymptotics')
+plt.plot(fr.hypos, [ fit_result['sampling_cls'] for fit_result in fit_results ], 'b'  , label = 'Sampling')
 plt.legend()
 
 plt.show()
 
-print('Asymptotics, CLsb : UL(95) =', find_hypo(hypo_mus, [ hd['cl']           for hd in hypo_dicts ]))
-print('Sampling,    CLsb : UL(95) =', find_hypo(hypo_mus, [ hd['sampling_cl']  for hd in hypo_dicts ]))
-print('Asymptotics, CLs  : UL(95) =', find_hypo(hypo_mus, [ hd['cls']          for hd in hypo_dicts ]))
-print('Sampling,    CLs  : UL(95) =', find_hypo(hypo_mus, [ hd['sampling_cls'] for hd in hypo_dicts ]))
+print('Asymptotics, CLsb : UL(95) =', find_hypo(fr.hypos, [ fit_result['cl']           for fit_result in fit_results ]))
+print('Sampling,    CLsb : UL(95) =', find_hypo(fr.hypos, [ fit_result['sampling_cl']  for fit_result in fit_results ]))
+print('Asymptotics, CLs  : UL(95) =', find_hypo(fr.hypos, [ fit_result['cls']          for fit_result in fit_results ]))
+print('Sampling,    CLs  : UL(95) =', find_hypo(fr.hypos, [ fit_result['sampling_cls'] for fit_result in fit_results ]))
