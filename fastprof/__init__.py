4e658ece5ede32ce7951823254415894e465b407
Nicolas Berger <nicolas.berger@cern.ch>
2020-04-29 02:56:11 +0200

    add minimizer imports

--- fastprof/__init__.py
+++ fastprof/__init__.py
@@ -1,5 +1,6 @@
 from .core            import Model, Parameters, Data
 from .test_statistics import QMu, TMu
+from .minimizers      import NPMinimizer, OptiMinimizer, ScanMinimizer
 from .sampling        import SamplingDistribution, Samples, CLsSamples
 from .samplers        import ScanSampler, OptiSampler, PyhfSampler, DebuggingScanSampler
 
