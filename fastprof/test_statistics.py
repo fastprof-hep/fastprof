7fe5c7c4ca6f5deb2e220d5d3359f192cedf9885
Nicolas Berger <nicolas.berger@cern.ch>
2020-05-01 02:52:53 +0200

    move to uncapped q

--- fastprof/test_statistics.py
+++ fastprof/test_statistics.py
@@ -27,12 +27,18 @@ class TMu(TestStatistic) :
 
 class QMu(TestStatistic) :
   def __init__(self, twice_dll, test_mu, best_mu) :
-    self.value = twice_dll if best_mu < test_mu else None
+    self.value = twice_dll if best_mu < test_mu else -twice_dll
     self.test_mu = test_mu
+
   def value(self) :
     return self.value
-  def asymptotic_cl(self) :
-    return scipy.stats.norm.sf(math.sqrt(self.value)) if (self.value != None and self.value > 0) else 0.5
+
+  def asymptotic_cl(self, offset = 0) :
+    if self.value >= 0 :
+      return scipy.stats.norm.sf(+math.sqrt(+self.value) - offset)
+    else :
+      return scipy.stats.norm.sf(-math.sqrt(-self.value) - offset)
+
   def asymptotic_cls(self, qA = None, sigma = None) :
     if qA != None :
       offset = math.sqrt(qA)
@@ -41,7 +47,7 @@ class QMu(TestStatistic) :
     else :
       raise ValueError('Should supply either qA or sigma for the asymptotic CL_s computation')
     clsb = self.asymptotic_cl()
-    cl_b = scipy.stats.norm.sf(math.sqrt(self.value) - offset) if (self.value != None and self.value > 0) else 0.5
+    cl_b = self.asymptotic_cl(offset)
     #print('Asymptotic CLs = %g/%g = %g' % (clsb, cl_b, clsb/cl_b))
-    return clsb/cl_b
+    return clsb/cl_b if cl_b > 0 else 1
 
