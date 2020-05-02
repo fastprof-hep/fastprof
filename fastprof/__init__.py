8470378e22a403aac5d5742b3e280e6e311d91b0
Nicolas Berger <nicolas.berger@cern.ch>
2020-05-02 17:47:50 +0200

    include new calculator

--- fastprof/__init__.py
+++ fastprof/__init__.py
@@ -3,4 +3,4 @@ from .test_statistics import QMu, TMu
 from .minimizers      import NPMinimizer, OptiMinimizer, ScanMinimizer
 from .sampling        import SamplingDistribution, Samples, CLsSamples
 from .samplers        import ScanSampler, OptiSampler, PyhfSampler, DebuggingScanSampler
-from .tools           import FitResults
+from .tools           import FitResults, QMuCalculator
