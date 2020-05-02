933723deccfe9b26b1b9b06917b698a8c35549c9
Nicolas Berger <nicolas.berger@cern.ch>
2020-05-02 17:50:27 +0200

    use new calculator interface

--- utils/compute_limits.py
+++ utils/compute_limits.py
@@ -8,87 +8,89 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import matplotlib.pyplot as plt
 import numpy as np
 
-from fastprof import Model, Data, QMu, Samples, CLsSamples, OptiSampler, OptiMinimizer, FitResults
+from fastprof import Model, Data, Samples, CLsSamples, OptiSampler, OptiMinimizer, FitResults, QMuCalculator
 
 ####################################################################################################################################
 ###
 
-def compute_limits() :
-  """convert """
-  
-  parser = ArgumentParser("compute_limits.py", formatter_class=ArgumentDefaultsHelpFormatter)
-  parser.description = __doc__
-  parser.add_argument("-m", "--model-file",     required=True,   help="Name of JSON file defining model", type=str)
-  parser.add_argument("-d", "--data-file",      default='',      help="Name of JSON file defining the dataset (optional, otherwise taken from model file)", type=str)
-  parser.add_argument("-f", "--fits-file",      required=True,   help="Name of JSON file containing full-model fit results", type=str)
-  parser.add_argument("-n", "--ntoys",          default=10000,   help="Name of output file", type=int)
-  parser.add_argument("-s", "--seed",           default='0',     help="Name of output file", type=int)
-  parser.add_argument("-o", "--output-file",    required=True,   help="Name of output file", type=str)
-  parser.add_argument("-%", "--print-freq",     default=1000,    help="Verbosity level", type=int)
-  parser.add_argument("-v", "--verbosity",      default=0,       help="Verbosity level", type=int)
+parser = ArgumentParser("compute_limits.py", formatter_class=ArgumentDefaultsHelpFormatter)
+parser.description = __doc__
+parser.add_argument("-m", "--model-file",     required=True,   help="Name of JSON file defining model", type=str)
+parser.add_argument("-f", "--fits-file",      required=True,   help="Name of JSON file containing full-model fit results", type=str)
+parser.add_argument("-n", "--ntoys",          default=10000,   help="Name of output file", type=int)
+parser.add_argument("-s", "--seed",           default='0',     help="Name of output file", type=int)
+parser.add_argument("-o", "--output-file",    required=True,   help="Name of output file", type=str)
+parser.add_argument("-%", "--print-freq",     default=1000,    help="Verbosity level", type=int)
+parser.add_argument("-d", "--data-file",      default='',      help="Perform checks using the dataset stored in the specified JSON file", type=str)
+parser.add_argument("-a", "--asimov",         default=None,    help="Perform checks using an Asimov dataset for the specified POI value", type=float)
+parser.add_argument("-v", "--verbosity",      default=0,       help="Verbosity level", type=int)
 
-  options = parser.parse_args()
-  if not options : 
-    parser.print_help()
-    return
+options = parser.parse_args()
+if not options :
+  parser.print_help()
+  sys.exit(0)
 
-  model = Model.create(options.model_file)
+model = Model.create(options.model_file)
+res = FitResults(options.fits_file)
+fit_results = res.fit_results
 
-  full_results = FitResults(options.fits_file)
-  fit_results = full_results.fit_results
-  fr = full_results
- 
-  if options.seed != None : np.random.seed(options.seed)
+if options.data_file :
+  data = Data(model).load(options.data_file)
+  if data == None : raise ValueError('No valid dataset definition found in file %s.' % options.data_file)
+  print('Using dataset stored in file %s.' % options.data_file)
+elif options.asimov != None :
+  data = Data(model).set_expected(model.expected_pars(options.asimov))
+  print('Using Asimov dataset with %s = %g.' % (res.poi_name, options.asimov))
+else :
+  print('Using dataset stored in file %s.' % options.model_file)
+  data = Data(model).load(options.model_file)
 
-  opti_samples = CLsSamples(
-    Samples(fr.hypos, OptiSampler(model, mu0=fr.poi_initial_value, bounds=(fr.poi_min, fr.poi_max), print_freq=options.print_freq)          , options.output_file),
-    Samples(fr.hypos, OptiSampler(model, mu0=fr.poi_initial_value, bounds=(fr.poi_min, fr.poi_max), print_freq=options.print_freq, gen_mu=0), options.output_file + '_clb')) \
-    .generate_and_save(options.ntoys)
+# Check the fastprof CLs against the ones in the reference: in principle this should match well,
+# otherwise it means what we generate isn't exactly comparable to the observation, which would be a problem...
+print('Check CL computed from fast model against those of the full model (a large difference would require to correct the sampling distributions) :')
+calc = QMuCalculator(OptiMinimizer(data, res.poi_initial_value, (res.poi_min, res.poi_max)), res)
+calc.fill_qcl('qmu', 'cl', 'cls')
+calc.fill_fast()
+res.print(verbosity=1)
 
-  full_results.fill() # fill asymptotic results
-  for fit_result in fit_results :
-    fit_result['sampling_cl' ] = opti_samples.clsb.cl(fit_result[fr.poi_name], fit_result['cl'])
-    fit_result['sampling_cls'] = opti_samples.cl     (fit_result[fr.poi_name], fit_result['cl'])
+if options.seed != None : np.random.seed(options.seed)
 
-  # Check the fastprof CLs against the ones in the reference: in principle this should match well,
-  # otherwise it means what we generate isn't exactly comparable to the observation, which would be a problem...
-  print('Check CL computed from fast model against those of the full model (a large difference would require to correct the sampling distributions) :')
-  if options.data_file == '' :
-    print('Loading data from %s.' % options.model_file)
-    data = Data(model).load(options.model_file)
-  else :
-    print('Loading data from %s.' % options.data_file)
-    data = Data(model).load(options.data_file)
-  full_results.check(data)
+opti_samples = CLsSamples(
+  Samples(res.hypos, OptiSampler(model, mu0=res.poi_initial_value, bounds=(res.poi_min, res.poi_max), print_freq=options.print_freq)          , options.output_file),
+  Samples(res.hypos, OptiSampler(model, mu0=res.poi_initial_value, bounds=(res.poi_min, res.poi_max), print_freq=options.print_freq, gen_mu=0), options.output_file + '_clb')) \
+  .generate_and_save(options.ntoys)
 
-  # Print results
-  print('Asymptotics, CLsb : UL(95) =', fr.solve('cl'          , 0.05, log_scale = True))
-  print('Sampling,    CLsb : UL(95) =', fr.solve('sampling_cl' , 0.05, log_scale = True))
-  print('Asymptotics, CLs  : UL(95) =', fr.solve('cls'         , 0.05, log_scale = True))
-  print('Sampling,    CLs  : UL(95) =', fr.solve('sampling_cls', 0.05, log_scale = True))
+for fit_result in fit_results :
+  fit_result['sampling_cl' ] = opti_samples.clsb.cl(fit_result[res.poi_name], fit_result['cl'])
+  fit_result['sampling_clb'] = opti_samples.cl_b.cl(fit_result[res.poi_name], fit_result['cl'])
+  fit_result['sampling_cls'] = opti_samples.cl     (fit_result[res.poi_name], fit_result['cl'])
+res.print(verbosity=1)
 
-  # Plot results
-  plt.ion()
-  fig1 = plt.figure(1)
-  plt.suptitle('$CL_{s+b}$')
-  plt.xlabel('$\mu$')
-  plt.ylabel('$CL_{s+b}$')
-  plt.plot(fr.hypos, [ fit_result['cl']          for fit_result in fit_results ], 'r:' , label = 'Asymptotics')
-  plt.plot(fr.hypos, [ fit_result['sampling_cl'] for fit_result in fit_results ], 'b'  , label = 'Sampling')
-  plt.legend()
-  
-  fig2 = plt.figure(2)
-  plt.suptitle('$CL_s$')
-  plt.xlabel('$\mu$')
-  plt.ylabel('$CL_s$')
-  opti_samples.plot_bands(2)
-  plt.plot(fr.hypos, [ fit_result['cls']          for fit_result in fit_results ], 'r:' , label = 'Asymptotics')
-  plt.plot(fr.hypos, [ fit_result['sampling_cls'] for fit_result in fit_results ], 'b'  , label = 'Sampling')
-  plt.legend()
-  plt.show()
+# Print results
+print('Asymptotics, CLsb : UL(95) =', res.solve('cl'          , 0.05, log_scale = True))
+print('Sampling,    CLsb : UL(95) =', res.solve('sampling_cl' , 0.05, log_scale = True))
+print('Asymptotics, CLs  : UL(95) =', res.solve('cls'         , 0.05, log_scale = True))
+print('Sampling,    CLs  : UL(95) =', res.solve('sampling_cls', 0.05, log_scale = True))
 
-  fig1.savefig(options.output_file + '_clsb.pdf')
-  fig2.savefig(options.output_file + '_cls.pdf')
+# Plot results
+plt.ion()
+fig1 = plt.figure(1)
+plt.suptitle('$CL_{s+b}$')
+plt.xlabel('$\mu$')
+plt.ylabel('$CL_{s+b}$')
+plt.plot(res.hypos, [ fit_result['cl']          for fit_result in fit_results ], 'r:' , label = 'Asymptotics')
+plt.plot(res.hypos, [ fit_result['sampling_cl'] for fit_result in fit_results ], 'b'  , label = 'Sampling')
+plt.legend()
 
+fig2 = plt.figure(2)
+plt.suptitle('$CL_s$')
+plt.xlabel('$\mu$')
+plt.ylabel('$CL_s$')
+opti_samples.plot_bands(2)
+plt.plot(res.hypos, [ fit_result['cls']          for fit_result in fit_results ], 'r:' , label = 'Asymptotics')
+plt.plot(res.hypos, [ fit_result['sampling_cls'] for fit_result in fit_results ], 'b'  , label = 'Sampling')
+plt.legend()
+plt.show()
 
-compute_limits()
+fig1.savefig(options.output_file + '_clsb.pdf')
+fig2.savefig(options.output_file + '_cls.pdf')
