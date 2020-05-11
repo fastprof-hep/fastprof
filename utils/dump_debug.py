10d3c27fb9b0259626338877a4b657436f9ca988
Nicolas Berger <nicolas.berger@cern.ch>
2020-05-11 14:07:02 +0200

    migrate to new pandas-based debug data

--- utils/dump_debug.py
+++ utils/dump_debug.py
@@ -1,41 +1,28 @@
+# Usage : python3 -i fastprof/examples/dump_samples.py <sample_file.npy>
+
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-
 import sys
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-fname=sys.argv[1]
-samples = np.load(fname)
+####################################################################################################################################
+###
 
-# Samples data:
-# 0, 1 : data bin contents (0=SR, 1=CR)
-# 2, 3 : aux measurement values of signal (2) and background (3) NPs
-# 4, 5, 6, 7 : fast-fit values of mu (4) signal (5) and background (6) pars + CLs+b (7)
-# 8, 9, 10, 11 : best-fit values of mu (8) signal (9) and background (10) pars  + CLsb @ mu=mu_min (11)
-# 12, 13, 14 :  mu_min (12) and best-fit values of signal (13) and background (14) pars
+parser = ArgumentParser("dump_samples.py", formatter_class=ArgumentDefaultsHelpFormatter)
+parser.description = __doc__
+parser.add_argument('filename'         , type=str, nargs=1  , help='Name of the npy file in which samples are stored')
+parser.add_argument("-b", "--nbins"    , type=int, default=100, help="Number of bins to use")
+parser.add_argument("-l", "--log-scale", action='store_true'  , help="Use log scale for plotting")
+
+options = parser.parse_args()
+if not options :
+  parser.print_help()
+  sys.exit(0)
+
+debug = pd.read_csv(options.filename[0])
 
 plt.ion()
-
-SR_pred      = 1.0*samples[:,8]*(1 + 0.2*samples[:,9]) + 1.0*(1 + 0.2*samples[:,10])
-SR_pred_fast = 1.0*samples[:,4]*(1 + 0.2*samples[:,5]) + 1.0*(1 + 0.2*samples[:,6])
-CR_pred      = 10.0*(1 + 0.2*samples[:,10])
-CR_pred_fast = 10.0*(1 + 0.2*samples[:,6])
-
-fig1, axs1 = plt.subplots(2, 2)
-axs1[0,0].hist2d(samples[:,0], SR_pred     , bins=(11,20), range=[[0, 10],[0, 10]], cmap=plt.cm.viridis)
-axs1[0,1].hist2d(samples[:,0], SR_pred_fast, bins=(11,20), range=[[0, 10],[0, 10]], cmap=plt.cm.viridis)
-axs1[1,0].hist2d(samples[:,1], CR_pred     , bins=(21,20), range=[[0, 20],[0, 20]])
-axs1[1,1].hist2d(samples[:,1], CR_pred_fast, bins=(21,20), range=[[0, 20],[0, 20]])
-fig1.show()
-
-fig2, axs2 = plt.subplots(2, 2)
-axs2[0,0].hist2d(samples[:, 8], samples[:, 4], bins=(20,20), range=[[0, 15],[0, 15]], cmap=plt.cm.viridis)
-axs2[0,1].hist2d(samples[:, 9], samples[:, 5], bins=(20,20), range=[[-3, 3],[-3,3]])
-axs2[1,0].hist2d(samples[:,10], samples[:, 6], bins=(20,20), range=[[-3, 3],[-3,3]])
-axs2[1,1].hist2d(samples[:, 7], samples[:,11], bins=(20,20), range=[[0, 0.5],[0, 0.5]])
-fig2.show()
-
-fig3, axs3 = plt.subplots(2, 2)
-axs3[0,0].hist2d(samples[:, 8], samples[:,12], bins=(15,15), range=[[0, 15],[0, 15]], cmap=plt.cm.viridis)
-axs3[0,1].hist2d(samples[:, 9], samples[:,13], bins=(20,20), range=[[-3, 3],[-3,3]])
-axs3[1,0].hist2d(samples[:,10], samples[:,14], bins=(20,20), range=[[-3, 3],[-3,3]])
-fig3.show()
+debug.hist('mu_hat',bins=options.nbins)
+if options.log_scale : plt.yscale('log')
+plt.show()
