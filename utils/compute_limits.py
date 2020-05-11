2bacc8398b92f34e8bfa16660157a8d6124f266b
Nicolas Berger <nicolas.berger@cern.ch>
2020-05-11 14:12:17 +0200

    move to QMuTilda, other minor improvements

--- utils/compute_limits.py
+++ utils/compute_limits.py
@@ -8,22 +8,27 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import matplotlib.pyplot as plt
 import numpy as np
 
-from fastprof import Model, Data, Samples, CLsSamples, OptiSampler, OptiMinimizer, FitResults, QMuCalculator
+from fastprof import Model, Data, Samples, CLsSamples, OptiSampler, OptiMinimizer, FitResults, QMuCalculator, QMuTildaCalculator
 
 ####################################################################################################################################
 ###
 
 parser = ArgumentParser("compute_limits.py", formatter_class=ArgumentDefaultsHelpFormatter)
 parser.description = __doc__
-parser.add_argument("-m", "--model-file",     required=True,   help="Name of JSON file defining model", type=str)
-parser.add_argument("-f", "--fits-file",      required=True,   help="Name of JSON file containing full-model fit results", type=str)
-parser.add_argument("-n", "--ntoys",          default=10000,   help="Name of output file", type=int)
-parser.add_argument("-s", "--seed",           default='0',     help="Name of output file", type=int)
-parser.add_argument("-o", "--output-file",    required=True,   help="Name of output file", type=str)
-parser.add_argument("-%", "--print-freq",     default=1000,    help="Verbosity level", type=int)
-parser.add_argument("-d", "--data-file",      default='',      help="Perform checks using the dataset stored in the specified JSON file", type=str)
-parser.add_argument("-a", "--asimov",         default=None,    help="Perform checks using an Asimov dataset for the specified POI value", type=float)
-parser.add_argument("-v", "--verbosity",      default=0,       help="Verbosity level", type=int)
+parser.add_argument("-m", "--model-file"    , type=str  , required=True      , help="Name of JSON file defining model")
+parser.add_argument("-f", "--fits-file"     , type=str  , required=True      , help="Name of JSON file containing full-model fit results")
+parser.add_argument("-n", "--ntoys"         , type=int  , default=10000      , help="Name of output file")
+parser.add_argument("-s", "--seed"          , type=int  , default='0'        , help="Name of output file")
+parser.add_argument("-o", "--output-file"   , type=str  , required=True      , help="Name of output file")
+parser.add_argument("-%", "--print-freq"    , type=int  , default=1000       , help="Verbosity level")
+parser.add_argument("-d", "--data-file"     , type=str  , default=''         , help="Perform checks using the dataset stored in the specified JSON file")
+parser.add_argument("-a", "--asimov"        , type=float, default=None       , help="Perform checks using an Asimov dataset for the specified POI value")
+parser.add_argument("-i", "--iterations"    , type=int  , default=1          , help="Numer of iterations to perform for NP computation")
+parser.add_argument("-r", "--regularize"    , type=float, default=None       , help="Set loose constraints at specified N_sigmas on free NPs to avoid flat directions")
+parser.add_argument("-t", "--test-statistic", type=str  , default='qmu_tilda', help="Test statistic to use")
+parser.add_argument("-b", "--break-locks"   , action='store_true'            , help="Allow breaking locks from other sample production jobs")
+parser.add_argument(      "--debug"         , action='store_true'            , help="Produce debugging output")
+parser.add_argument("-v", "--verbosity"     , type=int  , default=0          , help="Verbosity level")
 
 options = parser.parse_args()
 if not options :
@@ -31,6 +36,8 @@ if not options :
   sys.exit(0)
 
 model = Model.create(options.model_file)
+if options.regularize != None : model = model.regularize(options.regularize)
+
 res = FitResults(options.fits_file)
 fit_results = res.fit_results
 
@@ -48,17 +55,22 @@ else :
 # Check the fastprof CLs against the ones in the reference: in principle this should match well,
 # otherwise it means what we generate isn't exactly comparable to the observation, which would be a problem...
 print('Check CL computed from fast model against those of the full model (a large difference would require to correct the sampling distributions) :')
-calc = QMuCalculator(OptiMinimizer(data, res.poi_initial_value, (res.poi_min, res.poi_max)), res)
-calc.fill_qcl('qmu', 'cl', 'cls')
-calc.fill_fast()
+if options.test_statistic == 'qmu_tilda' :
+  calc = QMuCalculator(OptiMinimizer(data, res.poi_initial_value, (res.poi_min, res.poi_max)), res)
+elif options.test_statistic == 'qmu' :
+  calc = QMuCalculator(OptiMinimizer(data, res.poi_initial_value, (res.poi_min, res.poi_max)), res)
+else:
+  raise ValueError('Unknown test statistic %s.' % options.test_statistic)
+calc.fill_qcl()
+calc.fill_fast_results()
 res.print(verbosity=1)
 
 if options.seed != None : np.random.seed(options.seed)
 
 opti_samples = CLsSamples(
-  Samples(res.hypos, OptiSampler(model, mu0=res.poi_initial_value, bounds=(res.poi_min, res.poi_max), print_freq=options.print_freq)          , options.output_file),
-  Samples(res.hypos, OptiSampler(model, mu0=res.poi_initial_value, bounds=(res.poi_min, res.poi_max), print_freq=options.print_freq, gen_mu=0), options.output_file + '_clb')) \
-  .generate_and_save(options.ntoys)
+  Samples(res.hypos, OptiSampler(model, mu0=res.poi_initial_value, bounds=(res.poi_min, res.poi_max), print_freq=options.print_freq, debug=options.debug, niter=options.iterations)          , options.output_file),
+  Samples(res.hypos, OptiSampler(model, mu0=res.poi_initial_value, bounds=(res.poi_min, res.poi_max), print_freq=options.print_freq, debug=options.debug, niter=options.iterations, gen_mu=0), options.output_file + '_clb')) \
+  .generate_and_save(options.ntoys, break_locks=options.break_locks)
 
 for fit_result in fit_results :
   fit_result['sampling_cl' ] = opti_samples.clsb.cl(fit_result[res.poi_name], fit_result['cl'])
@@ -66,11 +78,16 @@ for fit_result in fit_results :
   fit_result['sampling_cls'] = opti_samples.cl     (fit_result[res.poi_name], fit_result['cl'])
 res.print(verbosity=1)
 
+limit_asymptot_clsb = res.solve('cl'          , 0.05, log_scale = True)
+limit_sampling_clsb = res.solve('sampling_cl' , 0.05, log_scale = True)
+limit_asymptot_cls  = res.solve('cls'         , 0.05, log_scale = True)
+limit_sampling_cls  = res.solve('sampling_cls', 0.05, log_scale = True)
+
 # Print results
-print('Asymptotics, CLsb : UL(95) =', res.solve('cl'          , 0.05, log_scale = True))
-print('Sampling,    CLsb : UL(95) =', res.solve('sampling_cl' , 0.05, log_scale = True))
-print('Asymptotics, CLs  : UL(95) =', res.solve('cls'         , 0.05, log_scale = True))
-print('Sampling,    CLs  : UL(95) =', res.solve('sampling_cls', 0.05, log_scale = True))
+print('Asymptotics, CLsb : UL(95) = %g (N_signal = %g)' % (limit_asymptot_clsb, np.sum(model.s_exp(model.expected_pars(limit_asymptot_clsb)))))
+print('Sampling,    CLsb : UL(95) = %g (N_signal = %g)' % (limit_sampling_clsb, np.sum(model.s_exp(model.expected_pars(limit_sampling_clsb)))))
+print('Asymptotics, CLs  : UL(95) = %g (N_signal = %g)' % (limit_asymptot_cls , np.sum(model.s_exp(model.expected_pars(limit_asymptot_cls )))))
+print('Sampling,    CLs  : UL(95) = %g (N_signal = %g)' % (limit_sampling_cls , np.sum(model.s_exp(model.expected_pars(limit_sampling_cls )))))
 
 # Plot results
 plt.ion()
