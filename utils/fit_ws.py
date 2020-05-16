d13f1a75e0cd283d5071f56de24dcd40092537b7
Nicolas Berger <nicolas.berger@cern.ch>
2020-05-17 01:15:39 +0200

    python3 migration: add () to print

--- utils/fit_ws.py
+++ utils/fit_ws.py
@@ -63,7 +63,7 @@ if options.setval != '' :
       if not ws.var(var) :
         raise ValueError("Cannot find variable '%s' in workspace" % var)
       ws.var(var).setVal(float(val))
-      print "INFO : setting %s=%g" % (var, float(val))
+      print("INFO : setting %s=%g" % (var, float(val)))
   except Exception as inst :
     print(inst)
     raise ValueError("ERROR : invalid variable assignment string '%s'." % options.setval)
@@ -73,12 +73,12 @@ if options.setconst != '' :
   for var in varlist :
     matching_vars = ROOT.RooArgList(ws.allVars().selectByName(var))
     if matching_vars.getSize() == 0 :
-      print "ERROR : no variables matching '%s' in model" % var
+      print("ERROR : no variables matching '%s' in model" % var)
       raise ValueError
     for i in range(0, matching_vars.getSize()) :
       thisvar =  matching_vars.at(i)
       thisvar.setConstant()
-      print "INFO : setting variable '%s' constant (current value: %g)" % (thisvar.GetName(), thisvar.getVal())
+      print("INFO : setting variable '%s' constant (current value: %g)" % (thisvar.GetName(), thisvar.getVal()))
 
 if options.poi_initial_value != None :
   poi.setVal(options.poi_initial_value)
