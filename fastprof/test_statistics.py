fe294831b3920adb1961db9060ed2b21ffc106a1
Nicolas Berger <nicolas.berger@cern.ch>
2020-05-22 14:03:29 +0200

    mu->poi, add tmu checks

--- fastprof/test_statistics.py
+++ fastprof/test_statistics.py
@@ -4,8 +4,8 @@ from abc import abstractmethod
 
 # -------------------------------------------------------------------------
 class TestStatistic :
-  def __init__(self, test_mu) : 
-    self.test_mu = test_mu
+  def __init__(self, test_poi) : 
+    self.test_poi = test_poi
   def __float__(self) :
     return value()
   @abstractmethod
@@ -17,8 +17,8 @@ class TestStatistic :
     
 
 class TMu(TestStatistic) :
-  def __init__(self, test_mu, twice_dll) :
-    super().__init__(test_mu)
+  def __init__(self, test_poi, twice_dll) :
+    super().__init__(test_poi)
     self.value = twice_dll
   def value(self) : 
     return self.value
@@ -27,23 +27,23 @@ class TMu(TestStatistic) :
 
 
 class QMu(TestStatistic) :
-  def __init__(self, test_mu, tmu, best_mu, comp_mu = None, tmu_A = None, sigma = None) :
-    super().__init__(test_mu)
+  def __init__(self, test_poi, tmu, best_poi, comp_poi = None, tmu_A = None, sigma = None) :
+    super().__init__(test_poi)
     self.tmu = tmu
-    self.best_mu = best_mu
-    self.comp_mu = comp_mu if comp_mu != None else self.test_mu
+    self.best_poi = best_poi
+    self.comp_poi = comp_poi if comp_poi != None else self.test_poi
     self.tmu_A = tmu_A
     self.sigma = sigma
 
   def value(self) :
-    return self.tmu if self.best_mu < self.comp_mu else -self.tmu
+    return self.tmu if self.best_poi < self.comp_poi else -self.tmu
 
   def non_centrality_parameter(self) :
-    if self.comp_mu == self.test_mu : return 0
+    if self.comp_poi == self.test_poi : return 0
     if self.tmu_A != None :
       return self.tmu_A
     elif self.sigma != None :
-      return ((self.comp_mu - self.test_mu)/self.sigma)**2
+      return ((self.comp_poi - self.test_poi)/self.sigma)**2
     else :
       raise ValueError('Should supply either tmu_A or sigma for the asymptotic CL_s computation')
 
@@ -54,7 +54,7 @@ class QMu(TestStatistic) :
       return scipy.stats.norm.sf(-math.sqrt(-self.value()) - math.sqrt(self.non_centrality_parameter()))
 
   def asymptotic_clb(self) :
-    return QMu(0, self.tmu, self.best_mu, self.comp_mu, self.tmu_A, self.sigma).asymptotic_cl()
+    return QMu(0, self.tmu, self.best_poi, self.comp_poi, self.tmu_A, self.sigma).asymptotic_cl()
 
   def asymptotic_cls(self) :
     clsb = self.asymptotic_cl()
@@ -64,22 +64,28 @@ class QMu(TestStatistic) :
 
 
 class QMuTilda(TestStatistic) :
-  def __init__(self, test_mu, tmu, best_mu, comp_mu = None, tmu_A = None, tmu_0 = None) :
-    super().__init__(test_mu)
+  def __init__(self, test_poi, tmu, best_poi, comp_poi = None, tmu_A = None, tmu_0 = None) :
+    super().__init__(test_poi)
     self.tmu = tmu
-    self.best_mu = best_mu
-    self.comp_mu = comp_mu if comp_mu != None else self.test_mu
-    self.tmu_A = tmu_A # corresponds to the test_mu hypo
+    self.best_poi = best_poi
+    self.comp_poi = comp_poi if comp_poi != None else self.test_poi
+    self.tmu_A = tmu_A # corresponds to the test_poi hypo
     self.tmu_0 = tmu_0 # corresponds to the mu=0 hypo
 
   def value(self) :
-    return self.tmu if self.best_mu < self.comp_mu else -self.tmu
+    return self.tmu if self.best_poi < self.comp_poi else -self.tmu
 
   def non_centrality_parameter(self) : # (mu - mu')^2/sigma^2
-    if self.comp_mu == self.test_mu : return 0
+    if self.comp_poi == self.test_poi : return 0
+    if self.tmu_A < 0 :
+      print('Warning: qmutilda tmu_A = % g < 0, returning 0' % self.tmu_A)
+      return 0
     return self.tmu_A
 
   def threshold(self) : # mu^2/sigma^2
+    if self.tmu_0 < 0 :
+      print('Warning: qmutilda threshold = % g < 0, returning 0' % self.tmu_0)
+      return 0
     return self.tmu_0
 
   def asymptotic_cl(self) :
@@ -92,7 +98,7 @@ class QMuTilda(TestStatistic) :
       return scipy.stats.norm.sf((self.value() + self.threshold())/(2*math.sqrt(self.threshold())) - math.sqrt(self.non_centrality_parameter()))
 
   def asymptotic_clb(self) :
-    return QMuTilda(0, self.tmu, self.best_mu, self.comp_mu, self.tmu_0, self.tmu_0).asymptotic_cl()
+    return QMuTilda(0, self.tmu, self.best_poi, self.comp_poi, self.tmu_0, self.tmu_0).asymptotic_cl()
 
   def asymptotic_cls(self) :
     clsb = self.asymptotic_cl()
