41faff707779b2569e17292fa03e21dbf2666b22
Nicolas Berger <nicolas.berger@cern.ch>
2020-05-22 14:00:12 +0200

    mu->poi, support minimizing around a hypo

--- fastprof/minimizers.py
+++ fastprof/minimizers.py
@@ -9,9 +9,9 @@ from .core import Model, Parameters, Data
 
 # -------------------------------------------------------------------------
 class NPMinimizer :
-  def __init__(self, mu, data) :
+  def __init__(self, poi, data) :
     self.model = data.model
-    self.mu = mu
+    self.poi = poi
     self.data = data
 
 # Same as above, more readable, and faster -- use by default
@@ -34,7 +34,7 @@ class NPMinimizer :
     return np.block([[pAA, pAB, pAC], [np.transpose(pAB), pBB, pBC], [np.transpose(pAC), np.transpose(pBC), pCC]]), np.block([qA, qB, qC])
   
   def profile(self, pars_nom = None) :
-    if pars_nom == None : pars_nom = self.model.expected_pars(self.mu).set_from_aux(self.data)
+    if pars_nom == None : pars_nom = self.model.expected_pars(self.poi).set_from_aux(self.data)
     p, q = self.pq_einsum(pars_nom)
     d = np.linalg.det(p)
     if abs(d) < 1E-8 :
@@ -43,8 +43,8 @@ class NPMinimizer :
     else :
       v = np.linalg.inv(p).dot(q)
       nps = pars_nom.alphas - v[:self.model.na], pars_nom.betas - v[self.model.na:self.model.nsyst], pars_nom.gammas - v[self.model.nsyst:]
-    self.min_pars = Parameters(self.mu, *nps, self.model)
-    return nps
+    self.min_pars = Parameters(self.poi, *nps, self.model)
+    return self.min_pars
   
   def profile_nll(self, pars_nom = None) :
     self.profile(pars_nom)
@@ -58,92 +58,104 @@ class POIMinimizer :
     self.data = data
     self.niter = niter
   @abstractmethod
-  def minimize(self) :
+  def minimize(self, hypo) :
     pass
-  def profile_nps(self, mu) :
-    self.np_min = NPMinimizer(mu, self.data)
-    self.min_pars = None
+  def profile_nps(self, hypo) :
+    if isinstance(hypo, (int, float)) : 
+      hypo = self.model.expected_pars(hypo)
+    self.np_min = NPMinimizer(hypo.poi, self.data)
+    self.min_pars = hypo
     for i in range(0, self.niter) :
       self.np_min.profile(self.min_pars)
       self.min_pars = self.np_min.min_pars
     self.nll_min = self.model.nll(self.min_pars, self.data)
-  def tmu(self, mu) :
-    self.minimize()
-    if self.nll_min == None : return None, None
-    self.free_nll = self.nll_min
-    self.free_pars = self.min_pars
-    self.profile_nps(mu)
+    #print('profile NPs @ %g' % poi)
+    #print(str(self.min_pars))
+    return self.min_pars
+  def tmu(self, hypo) :
+    if isinstance(hypo, (int, float)) :
+      hypo = self.model.expected_pars(hypo)
+    #print('tmu @ %g' % hypo.poi)
+    self.profile_nps(hypo)
     self.hypo_nll = self.nll_min
     self.hypo_pars = self.min_pars
-    return 2*(self.hypo_nll - self.free_nll), self.min_mu
-  def asimov_clone(self, mu) :
+    self.minimize(hypo)
+    if self.nll_min == None : return None
+    self.free_nll = self.nll_min
+    self.free_pars = self.min_pars
+    #print(self.free_nll, str(self.free_pars))
+    #print(self.hypo_nll, str(self.hypo_pars))
+    return 2*(self.hypo_nll - self.free_nll)
+  def asimov_clone(self, poi) :
     clone = copy.copy(self)
-    clone.data = Data(self.model).set_expected(self.model.expected_pars(mu))
+    #print('generating asimov')
+    clone.data = self.model.generate_expected(poi, self)
     return clone
 
 # -------------------------------------------------------------------------
 class ScanMinimizer (POIMinimizer) :
-  def __init__(self, data, scan_mus, niter=1) :
+  def __init__(self, data, scan_pois, niter=1) :
     super().__init__(data, niter)
-    self.scan_mus = scan_mus
+    self.scan_pois = scan_pois
     self.pars = []
-    for mu in scan_mus :
-      self.pars.append(Parameters(mu, self.data.aux_alphas, self.data.aux_betas, np.zeros(self.model.nc), self.model))
+    for poi in scan_pois :
+      self.pars.append(Parameters(poi, self.data.aux_alphas, self.data.aux_betas, np.zeros(self.model.nc), self.model))
 
-  def minimize(self) :
-    self.nlls = np.zeros(self.scan_mus.size)
-    for i in range(0, len(self.scan_mus)) :
-      np_min = NPMinimizer(self.scan_mus[i], self.data)
+  def minimize(self, hypo) :
+    self.nlls = np.zeros(self.scan_pois.size)
+    for i in range(0, len(self.scan_pois)) :
+      scan_hypo = copy.deepcopy(hypo).set_poi(self.scan_pois[i])
+      np_min = NPMinimizer(scan_hypo, self.data)
       self.nlls[i] = np_min.profile_nll()
       self.pars[i] = np_min.min_pars
-      #print('@mu(', i, ') =', mu, self.nlls[i], ahat, bhat)
-    smooth_nll = InterpolatedUnivariateSpline(self.scan_mus, self.nlls, k=4)
+      #print('@poi(', i, ') =', poi, self.nlls[i], ahat, bhat)
+    smooth_nll = InterpolatedUnivariateSpline(self.scan_pois, self.nlls, k=4)
     minima = smooth_nll.derivative().roots()
     self.nll_min = np.amin(self.nlls)
     self.min_idx = np.argmin(self.nlls)
-    self.min_mu  = self.scan_mus[self.min_idx]
+    self.min_poi  = self.scan_pois[self.min_idx]
     if len(minima) == 1 :
       interp_min = smooth_nll(minima[0])
       if interp_min < self.nll_min :
-        self.min_mu  = minima[0]
+        self.min_poi  = minima[0]
         self.nll_min = interp_min
-    self.min_pars = Parameters(self.min_mu, self.pars[self.min_idx].alphas, self.pars[self.min_idx].betas, self.pars[self.min_idx].gammas)
-    return self.nll_min, self.min_mu
+    self.min_pars = Parameters(self.min_poi, self.pars[self.min_idx].alphas, self.pars[self.min_idx].betas, self.pars[self.min_idx].gammas)
+    return self.nll_min, self.min_poi
 
 # -------------------------------------------------------------------------
 class OptiMinimizer (POIMinimizer) :
-  def __init__(self, data, mu0 = 1, bounds = (0, 999999), method = 'scalar', niter=1) :
+  def __init__(self, data, poi0 = 1, bounds = (0, 10), method = 'scalar', niter=1) :
     super().__init__(data, niter)
     self.np_min = None
-    self.mu0 = mu0
+    self.poi0 = poi0
     self.bounds = bounds
     self.method = method
    
-  def minimize(self) :    
-    def objective(mu) :
-      if isinstance(mu, np.ndarray) : mu = mu[0]
-      if isinstance(mu, np.ndarray) : mu = mu[0]
-      self.profile_nps(mu)
-      # print('== OptMinimizer: eval at %g -> %g' % (mu, self.nll_min))
+  def minimize(self, hypo) :
+    current_hypo = copy.deepcopy(hypo)
+    def objective(poi) :
+      if isinstance(poi, np.ndarray) : poi = poi[0]
+      if isinstance(poi, np.ndarray) : poi = poi[0]
+      self.profile_nps(current_hypo.set_poi(poi))
+      #print('== OptMinimizer: eval at %g -> %g' % (poi, self.nll_min))
       return self.nll_min
-    def jacobian(mu) :
-      if isinstance(mu, np.ndarray) : mu = mu[0]
-      if isinstance(mu, np.ndarray) : mu = mu[0]
-      self.profile_nps(mu)
+    def jacobian(poi) :
+      if isinstance(poi, np.ndarray) : poi = poi[0]
+      if isinstance(poi, np.ndarray) : poi = poi[0]
+      self.profile_nps(current_hypo.set_poi(poi))
       #print('jac out', self.np_min.model.grad_poi(self.np_min.min_pars, self.data))
       return np.array([ self.np_min.model.grad_poi(self.np_min.min_pars, self.data) ])
-    def hess_p(mu, v) :
-      if isinstance(mu, np.ndarray) : mu = mu[0]
-      if isinstance(mu, np.ndarray) : mu = mu[0]
-      self.profile_nps(mu)
+    def hess_p(poi, v) :
+      if isinstance(poi, np.ndarray) : poi = poi[0]
+      if isinstance(poi, np.ndarray) : poi = poi[0]
+      self.profile_nps(current_hypo.set_poi(poi))
       #print('hess out', self.np_min.model.hess_poi(self.np_min.min_pars, self.data)*v[0])
       return np.array([ self.np_min.model.hess_poi(self.np_min.min_pars, self.data)*v[0] ])
-    #print('Optimizer: start bounded ----------------')
-    # print('Optimizer: switch to non-scalar  ----------------')
+    #print('Optimizer: using scalar  ----------------')
     if self.method == 'scalar' :
       result = scipy.optimize.minimize_scalar(objective, bounds=self.bounds, method='bounded', options={'xatol': 1e-3 })
     else :
-      result = scipy.optimize.minimize(objective, x0=self.mu0, bounds=(self.bounds,), method='L-BFGS-B', jac=jacobian, hessp=hess_p, options={'gtol': 1e-3, 'ftol':1e-3, 'xtol':1e-3 })
+      result = scipy.optimize.minimize(objective, x0=self.poi0, bounds=(self.bounds,), method='L-BFGS-B', jac=jacobian, hessp=hess_p, options={'gtol': 1e-3, 'ftol':1e-3, 'xtol':1e-3 })
       if not result.success:
         result = scipy.optimize.minimize_scalar(objective, bounds=self.bounds, method='bounded', options={'xtol': 1e-3 })
     #print('Optimizer: done ----------------')
@@ -158,6 +170,7 @@ class OptiMinimizer (POIMinimizer) :
       if hasattr(result, 'message') : print('message =', result.message)
       return None, None
     self.nll_min = result.fun
-    self.min_mu = result.x
+    self.min_poi = result.x
     self.nfev = result.nfev
-    return self.nll_min, self.min_mu
+    #print(self.min_poi)
+    return self.nll_min, self.min_poi
