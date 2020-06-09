2627578a780cc0f748ad087a288c4878a8d1b854
Nicolas Berger <nicolas.berger@cern.ch>
2020-06-09 15:56:04 +0200

    more options

--- utils/plot.py
+++ utils/plot.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-__doc__ = "Perform a fit to a fast model"
+__doc__ = "Plot a model and dataset"
 __author__ = "Nicolas Berger <Nicolas.Berger@cern.ch"
 
 import os, sys
@@ -14,15 +14,18 @@ import math
 
 parser = ArgumentParser("plot.py", formatter_class=ArgumentDefaultsHelpFormatter)
 parser.description = __doc__
-parser.add_argument("-m", "--model-file", type=str  , required=True , help="Name of JSON file defining model", type=str)
-parser.add_argument("-d", "--data-file" , type=str  , default=''    , help="Name of JSON file defining the dataset (optional, otherwise taken from model file)", type=str)
-parser.add_argument("-a", "--asimov"    , type=float, default=None  , help="Fit an Asimov dataset for the specified POI value", type=float)
-parser.add_argument("-p", "--poi-value" , type=float, required=True , help="POI initial value", type=float)
-parser.add_argument("-r", "--poi-range" , type=str  , default='0,20', help="POI allowed range, in the form min,max", type=str)
-parser.add_argument(      "--profile"   , action='store_true'       , help="Perform a conditional fit for the provided POI value before plotting")
-parser.add_argument("-l", "--log-scale" , action='store_true'       , help="Use log scale for plotting")
-parser.add_argument("-s", "--variations", type=str  , default=''    , help="Plot variations for parameters par1=val1[:color],par2=val2[:color]... or a single value for all parameters")
-parser.add_argument("-v", "--verbosity" , type=int  , default=0     , help="Verbosity level", type=int)
+parser.add_argument("-m", "--model-file" , type=str  , required=True , help="Name of JSON file defining model")
+parser.add_argument("-d", "--data-file"  , type=str  , default=''    , help="Name of JSON file defining the dataset (optional, otherwise taken from model file)")
+parser.add_argument("-a", "--asimov"     , type=float, default=None  , help="Fit an Asimov dataset for the specified POI value")
+parser.add_argument("-p", "--poi-value"  , type=float, required=True , help="POI initial value")
+parser.add_argument("-r", "--poi-range"  , type=str  , default='0,20', help="POI allowed range, in the form min,max")
+parser.add_argument("-x", "--x-range"    , type=str  , default=''    , help="X-axis range, in the form min,max")
+parser.add_argument("-y", "--y-range"    , type=str  , default=''    , help="Y-axis range, in the form min,max")
+parser.add_argument(      "--profile"    , action='store_true'       , help="Perform a conditional fit for the provided POI value before plotting")
+parser.add_argument("-l", "--log-scale"  , action='store_true'       , help="Use log scale for plotting")
+parser.add_argument("-s", "--variations" , type=str  , default=''    , help="Plot variations for parameters par1=val1[:color],par2=val2[:color]... or a single value for all parameters")
+parser.add_argument("-o", "--output-file", type=str  , default=''    , help="Output file name")
+parser.add_argument("-v", "--verbosity"  , type=int  , default=0     , help="Verbosity level")
 
 options = parser.parse_args()
 if not options :
@@ -98,23 +101,40 @@ if varias != None :
   plt.figure(1)
   model.plot(pars, data=data, variations=varias)
   if options.log_scale : plt.yscale('log')
-  return
+else :
+  if model.na > 0 :
+    nc = math.ceil(math.sqrt(model.na))
+    fig_a, ax_a = plt.subplots(nrows=nc, ncols=nc)
+    for alpha, ax in zip(model.alphas, ax_a.flatten()) :
+      model.plot(pars, data=data, variations = [ (alpha, var_val, 'r'), (alpha, -var_val, 'r') ], canvas=ax)
+      if options.log_scale : ax.set_yscale('log')
+  if model.nb > 0 :
+    nc = math.ceil(math.sqrt(model.nb))
+    fig_b, ax_b = plt.subplots(nrows=nc, ncols=nc)
+    for beta, ax in zip(model.betas, ax_b.flatten()) :
+      model.plot(pars, data=data, variations = [ (beta, var_val, 'r'), (beta, -var_val, 'r.') ], canvas=ax)
+      if options.log_scale : ax.set_yscale('log')
+  if model.nc > 0 :
+    nc = math.ceil(math.sqrt(model.nc))
+    fig_c, ax_c = plt.subplots(nrows=nc, ncols=nc)
+    for gamma, ax in zip(model.gammas, ax_c.flatten()) :
+      model.plot(pars, data=data, variations = [ (gamma, var_val, 'g'), (gamma, -var_val, 'g') ], canvas=ax)
+      if options.log_scale : ax.set_yscale('log')
 
-if model.na > 0 :
-  nc = math.ceil(math.sqrt(model.na))
-  fig_a, ax_a = plt.subplots(nrows=nc, ncols=nc)
-  for alpha, ax in zip(model.alphas, ax_a.flatten()) :
-    model.plot(pars, data=data, variations = [ (alpha, var_val, 'r'), (alpha, -var_val, 'r') ], canvas=ax)
-    if options.log_scale : ax.set_yscale('log')
-if model.nb > 0 :
-  nc = math.ceil(math.sqrt(model.nb))
-  fig_b, ax_b = plt.subplots(nrows=nc, ncols=nc)
-  for beta, ax in zip(model.betas, ax_b.flatten()) :
-    model.plot(pars, data=data, variations = [ (beta, var_val, 'r'), (beta, -var_val, 'r.') ], canvas=ax)
-    if options.log_scale : ax.set_yscale('log')
-if model.nc > 0 :
-  nc = math.ceil(math.sqrt(model.nc))
-  fig_c, ax_c = plt.subplots(nrows=nc, ncols=nc)
-  for gamma, ax in zip(model.gammas, ax_c.flatten()) :
-    model.plot(pars, data=data, variations = [ (gamma, var_val, 'g'), (gamma, -var_val, 'g') ], canvas=ax)
-    if options.log_scale : ax.set_yscale('log')
+if options.x_range :
+  try:
+    x_min, x_max = [ float(p) for p in options.x_range.split(',') ]
+  except Exception as inst :
+    print(inst)
+    raise ValueError('Invalid X-axis range specification %s, expected x_min,x_max' % options.x_range)
+  plt.xlim(x_min, x_max)
+
+if options.y_range :
+  try:
+    y_min, y_max = [ float(p) for p in options.y_range.split(',') ]
+  except Exception as inst :
+    print(inst)
+    raise ValueError('Invalid Y-axis range specification %s, expected y_min,y_max' % options.y_range)
+  plt.ylim(y_min, y_max)
+
+if options.output_file != '' : plt.savefig(options.output_file)
