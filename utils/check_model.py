355cccb69371c37425b002c35a5c46e87f0340b6
Nicolas Berger <nicolas.berger@cern.ch>
2020-06-15 18:40:13 +0200

    add plots

--- utils/check_model.py
+++ utils/check_model.py
@@ -5,6 +5,7 @@ __author__ = "Nicolas Berger <Nicolas.Berger@cern.ch"
 
 import os, sys
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+import matplotlib.pyplot as plt
 import json
 
 from fastprof import Model, Data, FitResults, QMuCalculator, QMuTildaCalculator, OptiMinimizer
@@ -18,8 +19,10 @@ parser.add_argument("-m", "--model-file"    , type=str  , default=''    , help="
 parser.add_argument("-d", "--data-file"     , type=str  , default=''    , help="Name of JSON file defining the dataset (optional, otherwise taken from model file)")
 parser.add_argument("-a", "--asimov"        , type=float, default=None  , help="Fit an Asimov dataset for the specified POI value")
 parser.add_argument("-f", "--fits-file"     , type=str  , default=''    , help="Name of JSON file containing full-model fit results")
-parser.add_argument("-r", "--regularize"    , type=float, default=None  , help="Set loose constraints at specified N_sigmas on free NPs to avoid flat directions")
+parser.add_argument(      "--regularize"    , type=float, default=None  , help="Set loose constraints at specified N_sigmas on free NPs to avoid flat directions")
 parser.add_argument("-t", "--test-statistic", type=str  , default='q~mu', help="Test statistic to use in the check")
+parser.add_argument(      "--marker"        , type=str  , default=''    , help="Marker type for plots")
+parser.add_argument("-b", "--batch-mode"    , action='store_true'       , help="Batch mode: no plots shown")
 parser.add_argument("-v", "--verbosity"     , type=int  , default=0     , help="Verbosity level")
 
 options = parser.parse_args()
@@ -53,3 +56,25 @@ else :
 calc.fill_qpv()
 calc.fill_fast_results()
 results.print(verbosity = options.verbosity)
+
+# Plot results
+if not options.batch_mode :
+  plt.ion()
+  fig1 = plt.figure(1)
+  plt.suptitle('$CL_{s+b}$')
+  plt.xlabel(model.poi_name)
+  plt.ylabel('$CL_{s+b}$')
+  plt.plot(results.hypos, [ fit_result['pv']      for fit_result in results.fit_results ], options.marker + 'r:' , label = 'Full model')
+  plt.plot(results.hypos, [ fit_result['fast_pv'] for fit_result in results.fit_results ], options.marker + 'g-' , label = 'Fast model')
+  plt.legend()
+
+  fig2 = plt.figure(2)
+  plt.suptitle('$CL_s$')
+  plt.xlabel(model.poi_name)
+  plt.ylabel('$CL_s$')
+  plt.plot(results.hypos, [ fit_result['cls']      for fit_result in results.fit_results ], options.marker + 'r:' , label = 'Full model')
+  plt.plot(results.hypos, [ fit_result['fast_cls'] for fit_result in results.fit_results ], options.marker + 'g-' , label = 'Fast model')
+  plt.legend()
+  fig1.savefig(options.output_file + '_clsb.pdf')
+  fig2.savefig(options.output_file + '_cls.pdf')
+  plt.show()
