41cdf310639d768b9f57cc8aafc653361fde3bf5
Nicolas Berger <nicolas.berger@cern.ch>
2020-08-15 11:32:18 +0200

    utility to apply cuts to parameters

--- fastprof/__init__.py
+++ fastprof/__init__.py
@@ -3,4 +3,4 @@ from .test_statistics import TMu, QMu, QMuTilda
 from .minimizers      import NPMinimizer, OptiMinimizer, ScanMinimizer
 from .sampling        import SamplingDistribution, Samples, CLsSamples
 from .samplers        import ScanSampler, OptiSampler
-from .tools           import FitResults, QMuCalculator, QMuTildaCalculator
+from .tools           import FitResults, QMuCalculator, QMuTildaCalculator, ParBound
