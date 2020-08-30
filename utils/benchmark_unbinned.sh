29cc8fec81158cb26bed8e5471c43d40816f12df
Nicolas Berger <nicolas.berger@cern.ch>
2020-08-30 03:10:01 +0200

    update benchmarking framework

--- utils/benchmark_unbinned.sh
+++ utils/benchmark_unbinned.sh
@@ -1,3 +1,3 @@
 name=$1
-python3 -m cProfile -o unbinned_$name.prof fastprof/testing/test_unbinned_gen.py
+python3 -m cProfile -o unbinned_$name.prof testing/test_unbinned_gen.py
 pyprof2calltree -k -i unbinned_$name.prof
