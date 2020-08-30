df35d44f92739c061ca393648d4d5ad31b5f5cea
Nicolas Berger <nicolas.berger@cern.ch>
2020-08-30 03:11:18 +0200

    update benchmarking framework

--- testing/test_unbinned_gen.py
+++ testing/test_unbinned_gen.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from fastprof import Model, ScanSampler, OptiSampler
 
-model = Model.create('run/high_mass_gg_1700-100bins.json')
+model = Model.create('fastprof/models/highMass-1164.json')
 
 gen_mu = 3
 print('Will generate the following hypothesis: ', gen_mu)
 
 np.random.seed(131071)
-dist = OptiSampler(model, mu0=1, bounds=(0,20), method='scalar').generate(gen_mu, 2000)
+dist = OptiSampler(model, test_hypo=model.expected_pars(0.1), mu0=0.1, poi_bounds=(0,2), method='scalar').generate(1000)
 dist.save('test')
