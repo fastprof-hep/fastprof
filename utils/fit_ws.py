6d69773b6ef6bbf571a113db9eeb6227ab8e6d78
Nicolas Berger <nicolas.berger@cern.ch>
2020-09-03 23:13:05 +0200

    working (anew) version

--- utils/fit_ws.py
+++ utils/fit_ws.py
@@ -9,7 +9,6 @@ import numpy as np
 import math
 import json
 from scipy.stats import norm
-import collections
 import ROOT
 
 from workspace_tools import process_setvals, process_setranges, process_setconsts, fit, make_asimov, make_binned
@@ -24,7 +23,8 @@ parser.add_argument("-w", "--ws-name"          , type=str  , default='modelWS', 
 parser.add_argument("-m", "--model-config-name", type=str  , default='mconfig', help="Name of model config within the specified workspace")
 parser.add_argument("-d", "--data-name"        , type=str  , default=''       , help="Name of dataset object within the input workspace")
 parser.add_argument("-a", "--asimov"           , action="store_true"          , help="Fit an Asimov dataset")
-parser.add_argument("-y", "--hypos"            , type=str  , default=''       , help="Colon-separated list of POI hypothesis values")
+parser.add_argument("-y", "--hypos"            , type=str  , default=''       , help="List of POI hypothesis values (poi1=val1,poi2=val2:...)")
+parser.add_argument(      "--hypos-file"       , type=str  , default=None     , help="File containing POI hypothesis values")
 parser.add_argument(      "--fit-options"      , type=str  , default=''       , help="RooFit fit options to use")
 parser.add_argument(      "--binned"           , action="store_true"          , help="Use binned data")
 parser.add_argument(      "--input_bins"       , type=int  , default=0        , help="Number of bins to use when binning the input dataset")
@@ -61,14 +61,14 @@ mconfig = ws.obj(options.model_config_name)
 if not mconfig : raise KeyError('Model config %s not found in workspace.' % options.model_config_name)
 
 main_pdf = mconfig.GetPdf()
-poi_set = mconfig.GetParametersOfInterest()
+pois = mconfig.GetParametersOfInterest()
 
 if options.poi_min != None : 
-  for poi in poi_set : poi.setMin(options.poi_min)
+  for poi in pois : poi.setMin(options.poi_min)
 if options.poi_max != None :
-  for poi in poi_set : poi.setMax(options.poi_max)
+  for poi in pois : poi.setMax(options.poi_max)
 
-np_set = mconfig.GetNuisanceParameters().selectByAttrib('Constant', False)
+nps = mconfig.GetNuisanceParameters().selectByAttrib('Constant', False)
 extra_nps = ROOT.RooArgSet()
 
 if options.nps != '' :
@@ -81,8 +81,8 @@ if options.nps != '' :
     for i in range(0, matching_vars.getSize()) :
       extra_nps.add(matching_vars.at(i))
 
-ws.saveSnapshot('init', np_set)
-poi_init_vals = [ poi.getVal() for poi in poi_set ]
+ws.saveSnapshot('init_nps', nps)
+ws.saveSnapshot('init_poi', pois)
 
 
 # 2. Get the data and refit as needed
@@ -104,26 +104,24 @@ if data == None :
 if options.data_name != '' and options.asimov : 
   print('=== Generating the main dataset as an Asimov, fitted as below')
   data = make_asimov(options.asimov, mconfig, main_pdf, data)
-    
+
+if options.binned : 
+  unbinned_data = data
+  data = make_binned(data, ROOT.RooArgList(mconfig.GetObservables())[0], options.input_bins)
+
 poi.setVal(0)
 poi.setConstant(True)
 result_bkg_only = fit(main_pdf, data, robust=True)
 print('=== Generating an Asimov dataset with POI = 0 and NP values below:')
-np_set.Print('V')
+nps.Print('V')
 asimov0 = ROOT.RooStats.AsymptoticCalculator.MakeAsimovData(mconfig, ROOT.RooArgSet(), ROOT.RooArgSet())
-
-nll  = main_pdf.createNLL(data)
-nll0 = main_pdf.createNLL(asimov0)
 
 
 # 3. Define hypothesis values
 # ===========================
 
-try :
-  fits_file = open(options.hypos)
-  fits_dict = json.load(fits_file)
-  hypos = [ fr[poi.GetName()] for fr in fits_dict['fit_results'] ]
-except :
+hypos = None
+if options.hypos :
   try :
     nhypos = int(options.hypos)
     n1 = (nhypos + 1) // 6
@@ -132,14 +130,21 @@ except :
     hypos = None
   except:
     try :
-      hypos = [ float(h) for h in options.hypos.split(',') ]
+      hypo_specs = [ process_setval(spec, parse_only=True) for spec in options.hypos.split(':') ]
+      hypos = [ { var : val for var, val, save in hypo_spec } for hypo_spec in hypo_specs ]
     except Exception as inst :
       print(inst)
-      raise ValueError("Could not parse list of hypothesis values '%s' : expected comma-separated list of real values" % options.hypos)
+      raise ValueError("Could not parse list of hypothesis values '%s' : expected colon-separated list of variable assignments" % options.hypos)
+elif option.hypos_file :
+  fits_file = open(options.hypos)
+  fit_results_dict = json.load(fits_file)
+  hypos = [ results_dict['hypo'] for results_dict in fit_results_dict['results'] ]
 
 if hypos == None : # we need to auto-define them based on the POI uncertainty
-  if len(poi_set) > 1 : raise('Cannot auto-set hypotheses for more than 1 POI')
-  ws.loadSnapshot('init')
+  if len(pois) > 1 : raise('Cannot auto-set hypotheses for more than 1 POI')
+  ws.loadSnapshot('init_nps')
+  ws.loadSnapshot('init_pois')
+  nll0  = main_pdf.createNLL(asimov0)
   nSignal = ws.var(options.signal_yield)
   if nSignal == None :
     nSignal = ws.function(options.signal_yield)
@@ -149,7 +154,6 @@ if hypos == None : # we need to auto-define them based on the POI uncertainty
     pv = 0.05
     return (3*math.exp(0.5/3*i))*math.exp(-unc**2/3) + (1 -math.exp(-unc**2/3))*(i + norm.isf(pv*norm.cdf(i)))*np.sqrt(9 + unc**2)
   poi.setConstant(False)
-  poi.setVal(poi_init_val)
   fit(main_pdf, asimov0, robust=True)
   free_nll = nll0.getVal()
   free_val = poi.getVal()
@@ -165,9 +169,10 @@ if hypos == None : # we need to auto-define them based on the POI uncertainty
   poi2sig = nSignal.getVal()/poi.getVal()
   print('=== Asimov qA uncertainty = %g (fit uncertainty = %g) evaluated at POI hypo = %g (nSignal = %g)' % (sigma_A, poi.getError(), poi.getVal(), nSignal.getVal()))
   hypos_nS = np.array([ hypo_guess(i, sigma_A*poi2sig) for i in hypo_zs ])
-  hypos = hypos_nS/poi2sig
+  hypo_vals = hypos_nS/poi2sig
+  hypos = [ { poi : hypo_val } for hypo_val in hypo_vals ]
   print('=== Auto-defined the following hypotheses :')
-  print('  ' + '\n  '.join([ '%5g : Nsig = %10g, POI = %10g' % (h_z, h_n, h_p) for h_z, h_n, h_p in zip(hypo_zs, hypos_nS, hypos) ] ))
+  print('  ' + '\n  '.join([ '%5g : Nsig = %10g, POI = %10g' % (h_z, h_n, h_p) for h_z, h_n, h_p in zip(hypo_zs, hypos_nS, hypo_vals) ] ))
 
   if options.poi_min == None : 
     # Set min to the -5sigma hypothesis, should be enough...
@@ -179,64 +184,65 @@ if hypos == None : # we need to auto-define them based on the POI uncertainty
     poi.setMax(hypo_guess(20, sigma_A*poi2sig)/poi2sig)
     print('=== Auto-set POI max to %g' % poi.getMax())
 
-jdict = collections.OrderedDict()
-fit_results = []
+jdict = {}
 
-for hypo in hypos :
-  result = collections.OrderedDict()
-  result[poi.GetName()] = hypo
-  # Fit the data first
-  print('=== Fitting data to hypothesis %g' % hypo)
-  ws.loadSnapshot('init')
-  poi.setVal(hypo)
-  # Fixed-mu fit
-  poi.setConstant(True)
-  result_hypo = fit(main_pdf, data, robust=True)
-  result['nll_hypo'] = nll.getVal()
-  for p in np_set    : result['hypo_' + p.GetName()] = result_hypo.floatParsFinal().find(p.GetName()).getVal()
-  for p in extra_nps : result['hypo_' + p.GetName()] = ws.var(p.GetName()).getVal()
+def fitpar_dict(par, result) :
+  dic = {}
+  dic['value'] = result.floatParsFinal().find(par.GetName()).getVal()
+  dic['error'] = result.floatParsFinal().find(par.GetName()).getError()
+  dic['min_value'] = par.getMin()
+  dic['max_value'] = par.getMax()
+  dic['initial_value'] = result.floatParsInit().find(par.GetName()).getVal()
+  return dic
+
+def plr_spec(hypo, dataset) :
+  spec = {}
+  # Hypo
+  hypo_spec = { poi.GetName() : val for poi, val in hypo.items() }
+  spec['hypo'] = hypo
+  print('=== Fitting dataset %s to hypothesis %s' % (dataset.GetName(), str(hypo_spec)))
+  nll  = main_pdf.createNLL(dataset)
+  ws.loadSnapshot('init_nps')
+  ws.loadSnapshot('init_pois')
+  # Hypo
+  spec['hypo'] = hypo_spec
+  # Initial valueshypos
+  init_vals = {}
+  for p in pois : init_vals[p.GetName()] = p.getVal()
+  for p in nps  : init_vals[p.GetName()] = p.getVal()
+  spec['initial_values'] = init_vals
+  # Fixed-POI fit
+  hypo_fit_spec = {}
+  for poi in hypo :
+    poi.setVal(hypo[poi])
+    poi.setConstant(True)
+  result_hypo = fit(main_pdf, dataset, robust=True)
+  hypo_fit_spec['nll'] = nll.getVal()
+  hypo_fit_spec['fit_pars'] = {}
+  for p in nps       : hypo_fit_spec['fit_pars'][p.GetName()] = fitpar_dict(p, result_hypo)
+  for p in extra_nps : hypo_fit_spec['fit_pars'][p.GetName()] = { 'value' : ws.var(p.GetName()).getVal() }
+  spec['hypo_fit'] = hypo_fit_spec
   # Free-mu fit
-  poi.setConstant(False)
-  result_free = fit(main_pdf, data, robust=True)
+  free_fit_spec = {}
+  for poi in hypo : poi.setConstant(False)
+  result_free = fit(main_pdf, dataset, robust=True)
   result_free.floatParsFinal().Print("V")
-  result['nll_free'] = nll.getVal()
-  result['fit_val'] = poi.getVal()
-  result['fit_err'] = poi.getError()
-  for p in np_set    : result['free_' + p.GetName()] = result_free.floatParsFinal().find(p.GetName()).getVal()
-  for p in extra_nps : result['free_' + p.GetName()] = ws.var(p.GetName()).getVal()
-  # Repeat for Asimov0
-  print('=== Fitting Asimov to hypothesis %g' % hypo)
-  ws.loadSnapshot('init')
-  poi.setVal(hypo)
-  # Fixed-mu fit
-  poi.setConstant(True)
-  result0_hypo = fit(main_pdf, asimov0, robust=True)
-  result['nll0_hypo'] = nll0.getVal()
-  # Free-mu fit
-  poi.setConstant(False)
-  result0_free = fit(main_pdf, asimov0, robust=True)
-  result['nll0_free'] = nll0.getVal()
-  result0_free.floatParsFinal().Print("V")
-  # Store results
-  fit_results.append(result)
+  free_fit_spec['nll'] = nll.getVal()
+  free_fit_spec['fit_pars'] = {}
+  for p in pois      : free_fit_spec['fit_pars'][p.GetName()] = fitpar_dict(p, result_free)
+  for p in nps       : free_fit_spec['fit_pars'][p.GetName()] = fitpar_dict(p, result_free)
+  for p in extra_nps : free_fit_spec['fit_pars'][p.GetName()] = { 'value' : ws.var(p.GetName()).getVal() }
+  spec['free_fit'] = free_fit_spec
+  return spec
 
-nlls = np.array([ result['nll_free'] for result in fit_results ])
-nll_best = np.amin(nlls)
-nll0s = np.array([ result['nll0_free'] for result in fit_results ])
-nll0_best = np.amin(nll0s)
-best_fit_val = fit_results[np.argmin(nlls)]['fit_val']
-best_fit_err = fit_results[np.argmin(nlls)]['fit_err']
-for result in fit_results :
-  result['nll_best']        = nll_best
-  result['best_fit_val']    = best_fit_val
-  result['best_fit_err']    = best_fit_err
-  result['tmu']             = 2*(result['nll_hypo'] - nll_best)
-  result['nll0_best']       = nll0_best
-  result['tmu_0']           = 2*(result['nll0_hypo'] - nll0_best)
+data_specs = []
+asimov_specs = []
+for hypo in hypos :
+  data_specs.append(plr_spec(hypo, data))
+  asimov_specs.append(plr_spec(hypo, asimov0))
 
-jdict['poi_name'] = poi.GetName()
-jdict['poi_initial_value'] = poi_init_val
-jdict['poi_range'] = poi.getMin(), poi.getMax()
-jdict['fit_results'] = fit_results
+jdict['data'] = data_specs
+jdict['asimov'] = asimov_specs
+print(jdict)
 with open(options.output_file, 'w') as fd:
   json.dump(jdict, fd, ensure_ascii=True, indent=3)
