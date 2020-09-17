14f48e1d57136bd65844643b8aafceb4233a1c03
Nicolas Berger <nicolas.berger@cern.ch>
2020-09-17 16:21:04 +0200

    propagate sampling errors to full-spectrum output

--- utils/collect_results.py
+++ utils/collect_results.py
@@ -3,6 +3,21 @@
 __doc__ = "Ccompute limits from sampling distributions"
 __author__ = "Nicolas Berger <Nicolas.Berger@cern.ch"
 
+# In ROOT, draw the output as follows
+#
+# - Simple
+# limit_sampling_CLs->Draw("CA")
+#
+# - With errors
+# limit_sampling_CLs->Draw("A4")
+# limit_sampling_CLs->Draw("LX")
+#
+# - Bands
+# limit_sampling_CLs_expected_band_2->Draw("A4")
+# limit_sampling_CLs_expected_band_1->Draw("L4SAME")
+# limit_sampling_CLs_expected_band_1->Draw("LX")
+
+
 import os, sys
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import copy
@@ -20,6 +35,7 @@ parser.add_argument("-p", "--positions"    , type=str  , default=''  , help="Par
 parser.add_argument("-i", "--input-pattern", type=str  , default=''  , help="Pattern of result files to load, with the value indicated by a * or a %")
 parser.add_argument("-k", "--key"          , type=str  , default=''  , help="Key indexing the output result")
 parser.add_argument("-b", "--bands"        , type=int  , default=None, help="Number of expected limit bands to include")
+parser.add_argument("-e", "--errors"       , action='store_true'     , help="Include sampling uncertainties on the limit values")
 parser.add_argument("-o", "--output-file"  , type=str  , default=''  , help="Output file name")
 parser.add_argument("-r", "--root-output"  , type=str  , default=''  , help="Output a ROOT file with the specified name")
 parser.add_argument("-l", "--log-scale"    , action='store_true'     , help="Use log scale for plotting")
@@ -82,12 +98,30 @@ for pos in positions :
   else :
     try :
       res = jdict[options.key]
-      if res == None : raise ValueError('Result is None')
-      results.append(float(res))
+      if res == None :
+        print('Result at position %g is None, skipping' % pos)
+        continue
     except Exception as inst :
       print(inst)
       print('Floating-point result not found at key %s in file %s. Available keys:\n%s' % (options.key, filename, '\n'.join(jdict.keys())))
       continue
+    if options.errors :
+      try :
+        resup = jdict[options.key + '_up']
+        resdn = jdict[options.key + '_dn']
+        if resup == None :
+          print('Positive error at position %g is None, skipping' % pos)
+          continue
+        if resdn == None :
+          print('Negative error at position %g is None, skipping' % pos)
+          continue
+      except Exception as inst :
+        print(inst)
+        print('Positive and negative errors on result not found at key %s in file %s. Available keys:\n%s' % (options.key, filename, '\n'.join(jdict.keys())))
+        continue
+      results.append([ float(res), float(resup), float(resdn) ])
+    else :
+      results.append(float(res))
   good_pos.append(pos)
   
 jdict = {}
@@ -99,41 +133,47 @@ with open(options.output_file, 'w') as fd:
 
 if options.root_output :
   import ROOT
+  colors = [ 1, 8, 5 ]
   if options.bands :
-    colors = [ 1, 8, 5 ]
-    taes = {}
+    gs = {}
     for band in np.linspace(-options.bands, options.bands, 2*options.bands + 1) :
       for band in range(1, options.bands + 1) :
-        tae = ROOT.TGraphAsymmErrors(len(good_pos))
-        tae.SetName(options.key + '_expected_band_%d' % band)
-        tae.SetTitle('')
-        tae.SetLineWidth(2)
-        tae.SetLineColor(1)
-        tae.SetLineStyle(2)
-        tae.SetFillColor(colors[band])
-        taes[band] = tae
+        g = ROOT.TGraphAsymmErrors(len(good_pos))
+        g.SetName(options.key + '_expected_band_%d' % band)
+        g.SetTitle('')
+        g.SetLineWidth(2)
+        g.SetLineColor(1)
+        g.SetLineStyle(2)
+        g.SetFillColor(colors[band])
+        gs[band] = g
+  elif options.errors :
+    g = ROOT.TGraphAsymmErrors(len(good_pos))
+    g.SetName(options.key)
+    g.SetLineWidth(2)
+    g.SetLineColor(1)
+    g.SetFillColor(colors[1])
   else :
     g = ROOT.TGraph(len(good_pos))
     g.SetName(options.key)
   for i in range(0, len(good_pos)) : 
     if options.bands :
       for band in range(1, options.bands + 1) :
-        taes[band].SetPoint(i, good_pos[i], results[0][i])
-        taes[band].SetPointEYhigh(i, results[band][i] - results[0][i])
-        taes[band].SetPointEYlow (i, results[0][i] - results[-band][i])
+        gs[band].SetPoint(i, good_pos[i], results[0][i])
+        gs[band].SetPointEYhigh(i, results[band][i] - results[0][i])
+        gs[band].SetPointEYlow (i, results[0][i] - results[-band][i])
+    elif options.errors :
+      g.SetPoint(i, good_pos[i], results[i][0])
+      g.SetPointEYhigh(i, results[i][1] - results[i][0])
+      g.SetPointEYlow (i, results[i][0] - results[i][2])
     else :
       g.SetPoint(i, good_pos[i], results[i])
   f = ROOT.TFile.Open(options.root_output, 'RECREATE')
   if options.bands :
     for band in range(1, options.bands + 1) :
-      taes[band].Write()
+      gs[band].Write()
   else :
     g.Write()
   f.Close()
-# In ROOT, draw as follows
-# limit_sampling_CLs_expected_band_2->Draw("AL4")
-# limit_sampling_CLs_expected_band_1->Draw("L4SAME")
-# limit_sampling_CLs_expected_band_1->Draw("LX")
 
 plt.ion()
 if options.log_scale : plt.yscale('log')
