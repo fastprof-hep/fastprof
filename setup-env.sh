0af16feee22d7ed3066d84f57bc48992d954fb05
Nicolas Berger <nicolas.berger@cern.ch>
2020-09-27 15:31:18 +0200

    install theme for documentation as part of the setup

--- setup-env.sh
+++ setup-env.sh
@@ -1,4 +1,4 @@
 python3 -m venv .
 source bin/activate
-pip install -q numpy scipy pandas matplotlib
+pip install -q numpy scipy pandas matplotlib pip sphinx-rtd-theme
 export PYTHONPATH=$PYTHONPATH:`pwd`
