75be0ce965ae3a08bdb78371d31f524547453269
Nicolas Berger <nicolas.berger@cern.ch>
2020-09-29 20:10:19 +0200

    move env to subdir, add scripts

--- setup-env.sh
+++ setup-env.sh
@@ -1,6 +1,3 @@
-python3 -m venv .
-source bin/activate
+python3 -m venv env
+source env/bin/activate
 python setup.py install
-#pip install --editable .
-#pip install -q numpy scipy pandas matplotlib sphinx-rtd-theme
-#export PYTHONPATH=$PYTHONPATH:`pwd`
--- setup.py
+++ setup.py
@@ -8,7 +8,18 @@ setup(
   author_email     = '',
   packages         = [ 'fastprof' ],
   install_requires = [ 'wheel', 'numpy', 'scipy', 'pandas', 'matplotlib', 'sphinx-rtd-theme' ],
-  scripts          = [ 'utils/compute_limits.py' ]
+  scripts          = [ 
+    'utils/convert_ws.py',
+    'utils/fit_ws.py',
+    'utils/plot_valid.py',
+    'utils/check_model.py',
+    'utils/fit_fast.py',
+    'utils/plot.py',
+    'utils/make_inputs.py',
+    'utils/compute_limits.py',
+    'utils/dump_samples.py',
+    'utils/collect_results.py'
+    ]
 #  entry_points     = {
 #    'console_scripts' : {
 #      'convert_ws.py = utils.convert_ws',
