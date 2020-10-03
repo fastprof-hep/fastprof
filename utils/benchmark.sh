2a2608f8bce81c597c5dd7257d9576bac3c55b25
Nicolas Berger <nicolas.berger@cern.ch>
2020-10-04 01:04:55 +0200

    path change

--- utils/benchmark.sh
+++ utils/benchmark.sh
@@ -1,3 +1,3 @@
 name=$1
-python3 -m cProfile -o benchmarks/fast_$name.prof fastprof/testing/test_sample_gen.py
+python3 -m cProfile -o benchmarks/fast_$name.prof testing/test_sample_gen.py
 pyprof2calltree -k -i benchmarks/fast_$name.prof
