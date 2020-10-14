a3b279aedcac684604d18cdf75f6d1c4291b94fc
Nicolas Berger <nicolas.berger@cern.ch>
2020-10-15 00:55:16 +0200

    move to entry_points

--- setup.py
+++ setup.py
@@ -6,20 +6,24 @@ setup(
   description      = 'A fast profiling tool for likelihood-based statistical analyses',
   author           = 'N. Berger',
   author_email     = 'nicolas.berger@cern.ch',
-  packages         = [ 'fastprof', 'root_import' ],
+  packages         = [ 'fastprof', 'root_import', 'utils' ],
   install_requires = [ 'numpy', 'scipy', 'pandas', 'matplotlib', 'sphinx-rtd-theme', 'sphinx-argparse' ],
+  entry_points = {
+    'console_scripts': [
+      'convert_ws.py      = root_import.convert_ws:run',
+      'fit_ws.py          = root_import.fit_ws:run',
+      'plot.py            = utils.plot:run',
+      'plot_valid.py      = utils.plot_valid:run',
+      'check_model.py     = utils.check_model:run',
+      'fit_fast.py        = utils.fit_fast:run',
+      'make_inputs.py     = utils.make_inputs:run',
+      'compute_limits.py  = utils.compute_limits:run',
+      'dump_samples.py    = utils.dump_samples:run',
+      'iterate.py         = utils.iterate:run',
+      'collect_results.py = utils.collect_results:run'
+    ],
+  },
   scripts          = [
-    'root_import/convert_ws.py',
-    'root_import/fit_ws.py',
-    'utils/plot_valid.py',
-    'utils/check_model.py',
-    'utils/fit_fast.py',
-    'utils/plot.py',
-    'utils/make_inputs.py',
-    'utils/compute_limits.py',
-    'utils/dump_samples.py',
-    'utils/iterate.py',
-    'utils/collect_results.py'
     ],
   setup_requires = [ 'wheel', 'sphinx' ],
 )
