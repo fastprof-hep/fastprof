3e10b1f36d7ef9e716369728d064b96e92f0b07d
Nicolas Berger <nicolas.berger@cern.ch>
2021-05-28 13:38:40 +0200

    add support for 2D scans

--- utils/poi_scan.py
+++ utils/poi_scan.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import json
 
-from fastprof import Model, Data, Parameters, OptiMinimizer, Raster, TMuCalculator, ParBound, PLRScan
+from fastprof import Model, Data, Parameters, OptiMinimizer, Raster, TMuCalculator, ParBound, PLRScan1D, PLRScan2D
 from utils import process_setval_list
 
 
@@ -88,37 +88,45 @@ def run(argv = None) :
       print(inst)
       raise ValueError('Could not parse parameter bound specification "%s", expected in the form name1=[min]#[max],name2=[min]#[max],...' % options.bounds)
 
-
   # Compute the tmu values
-  if len(model.pois) > 1 : raise ValueError('Currently not supporting more than 1 POI for this operation')
+  if len(model.pois) > 2 : raise ValueError('Currently not supporting more than 2 POIs for this operation')
   calc = TMuCalculator(OptiMinimizer(niter=options.iterations).set_pois_from_model(model, par_bounds))
   print('Producing PLR scan with POI(s) %s, bounds %s and niter=%d.' % (str(calc.minimizer.free_pois()), str(calc.minimizer.bounds), calc.minimizer.niter))
-  poi_name = calc.minimizer.free_pois()[0]
-  if len(calc.minimizer.free_pois()) > 1 : raise ValueError('Currently not supporting more than 1 POI for this operation')
+  if len(calc.minimizer.free_pois()) > 2 : raise ValueError('Currently not supporting more than 2 POIs for this operation')
   raster = calc.compute_fast_results(hypos, data)
-  #print(raster)
 
-  raster.print(keys=[ 'tmu', 'pv' ], verbosity=1)
-
-  poi_scan = PLRScan(raster, 'tmu', name='PLR Scan for %s' % poi_name, nsigmas=options.nsigmas, cl=options.cl)
-  interval = poi_scan.interval(print_result=True)
-  if interval is None : return
-  # Plot results
-  if not options.batch_mode :
-    plt.ion()
-    fig1 = plt.figure(1)
-    poi_scan.plot(plt, marker=options.marker + 'b-', label='PRL', smooth=100)
-    plt.ylim(0, None)
-    plt.axhline(y=1 - poi_scan.cl, color='k', linestyle='dotted')
-    plt.show()
-
+  raster.print(keys=[ 'tmu' ], verbosity=1)
   jdict = {}
-  jdict['cl'] = poi_scan.cl
-  jdict['poi_name'] = poi_name
-  jdict['poi_unit'] = model.pois[poi_name].unit
-  jdict['central_value']  = interval[0]
-  jdict['uncertainty_up'] = interval[1]
-  jdict['uncertainty_dn'] = interval[2]
+
+  if len(model.pois) == 1 :
+    poi_name = calc.minimizer.free_pois()[0]
+    poi_scan = PLRScan1D(raster, 'tmu', name='PLR Scan for %s' % poi_name, ts_name='t_{\mu}', nsigmas=options.nsigmas, cl=options.cl)
+    interval = poi_scan.interval(print_result=True)
+    if interval is None : return
+    # Plot results
+    if not options.batch_mode :
+      plt.ion()
+      fig1 = plt.figure(1)
+      poi_scan.plot(plt, marker=options.marker + 'b-', label='PRL', smooth=100)
+      plt.ylim(0, None)
+      plt.axhline(y=poi_scan.ts_level, color='k', linestyle='dotted')
+      plt.show()
+    jdict = {}
+    jdict['cl'] = poi_scan.cl()
+    jdict['poi_name'] = poi_name
+    jdict['poi_unit'] = model.pois[poi_name].unit
+    jdict['central_value']  = interval[0]
+    jdict['uncertainty_up'] = interval[1]
+    jdict['uncertainty_dn'] = interval[2]
+  else :
+    poi1_name = calc.minimizer.free_pois()[0]
+    poi2_name = calc.minimizer.free_pois()[1]
+    poi_scan = PLRScan2D(raster, 'tmu', name='PLR Scan for (%s,%s)' % (poi1_name, poi2_name), ts_name='t_{\mu}', nsigmas=options.nsigmas, cl=options.cl)
+    if not options.batch_mode :
+      plt.ion()
+      fig1 = plt.figure(1)
+      poi_scan.plot(plt, label='PRL')
+      plt.show()
 
   with open(options.output_file + '_results.json', 'w') as fd:
     json.dump(jdict, fd, ensure_ascii=True, indent=3)
