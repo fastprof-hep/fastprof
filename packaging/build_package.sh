6d8b2baf3da2c84fde94edc7a4ffcd5642c97673
Nicolas Berger <nicolas.berger@cern.ch>
2021-11-07 23:14:50 +0100

    fix directory

--- packaging/build_package.sh
+++ packaging/build_package.sh
@@ -1,4 +1,9 @@
-mkdir -p src tests
-cp -rf ../fastprof ../utils ../import src
-cp -f ../LICENSE .
-cp -f ../README.md .
+rm -rf fastprof
+mkdir -p fastprof/src fastprof/tests
+cp -rf ../fastprof ../utils ../import fastprof/src
+cp -f pyproject.toml fastprof
+cp -f setup.cfg fastprof
+cp -f setup.py fastprof
+cp -f ../LICENSE fastprof
+cp -f ../LICENSE fastprof
+cp -f ../README.md fastprof
