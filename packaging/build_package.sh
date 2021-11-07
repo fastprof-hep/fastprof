dcfae77b4ef7131680522d469319cf997b05ef63
Nicolas Berger <nicolas.berger@cern.ch>
2021-11-07 23:53:14 +0100

    revert setup file locations

--- packaging/build_package.sh
+++ packaging/build_package.sh
@@ -1,9 +1,9 @@
 rm -rf fastprof
 mkdir -p fastprof/src fastprof/tests
 cp -rf ../fastprof ../fastprof_utils ../fastprof_import fastprof/src
-cp -f pyproject.toml fastprof
-cp -f setup.cfg fastprof
-cp -f setup.py fastprof
+cp -f ../pyproject.toml fastprof
+cp -f ../setup.cfg fastprof
+cp -f ../setup.py fastprof
 cp -f ../LICENSE fastprof
 cp -f ../LICENSE fastprof
 cp -f ../README.md fastprof
