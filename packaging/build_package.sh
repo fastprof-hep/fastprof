a5b5891f2a3583ecd8b3686568749967d059b2f4
Nicolas Berger <nicolas.berger@cern.ch>
2021-11-08 00:09:50 +0100

    improve packaging, bump version number

--- packaging/build_package.sh
+++ packaging/build_package.sh
@@ -1,13 +1,2 @@
-rm -rf fastprof
-mkdir -p fastprof/src fastprof/tests
-cp -rf ../fastprof ../fastprof_utils ../fastprof_import fastprof/src
-cp -f ../pyproject.toml fastprof
-cp -f ../setup.cfg fastprof
-cp -f ../setup.py fastprof
-cp -f ../LICENSE fastprof
-cp -f ../LICENSE fastprof
-cp -f ../README.md fastprof
-
-cd fastprof
 python3 -m build
 
