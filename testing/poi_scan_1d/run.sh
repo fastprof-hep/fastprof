0d29da4dcc2469d0efeb61b13e76e6445f9ec5c9
Nicolas Berger <nicolas.berger@cern.ch>
2021-11-08 00:33:21 +0100

    update test paths

--- testing/poi_scan_1d/run.sh
+++ testing/poi_scan_1d/run.sh
@@ -1,3 +1,3 @@
-python utils/poi_scan.py \
+poi_scan.py \
    -m examples/high_stats_one_bin.json \
    -y xs_BSM=6:14:17+ -o poi_scan_1d -c 2
--- testing/poi_scan_2d/run.sh
+++ testing/poi_scan_2d/run.sh
@@ -1,3 +1,3 @@
-python utils/poi_scan.py \
+poi_scan.py \
     -m examples/high_stats_two_bins.json \
     -y xs1=6:14:17+,xs2=6:14:17+ -o poi_scan_2d
