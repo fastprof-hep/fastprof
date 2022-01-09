fix broadcasting error in hessian, and change gradient to match new syntax

src/fastprof/sample.py
@@ -272,7 +272,7 @@ class Sample(Serializable) :
          normalization factor value
     """
     norm_grad = self.norm.gradient(pois, reals, real_vals)
-    return np.outer(self.nominal_yields, norm_grad/self.nominal_norm) if norm_grad is not None else None    
+    return self.nominal_yields[:,None]*norm_grad/self.nominal_norm if norm_grad is not None else None    
 
   def hessian(self, pois : dict, reals : dict, real_vals : dict) -> np.array :
     """Computes the overall normalization factor
@@ -283,7 +283,7 @@ class Sample(Serializable) :
          normalization factor value
     """
     norm_hess = self.norm.hessian(pois, reals, real_vals)
-    return np.outer(self.nominal_yields, norm_hess/self.nominal_norm) if norm_hess is not None else None    
+    return self.nominal_yields[:,None,None]*norm_hess/self.nominal_norm if norm_hess is not None else None
 
   def __str__(self) -> str :
     """Provides a description string