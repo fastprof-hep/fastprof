857a4228d68624171e04ac41e387b4445f2ae79d
Nicolas Berger <nicolas.berger@cern.ch>
2022-01-15 01:33:03 +0100

    revert multi_channel example, used by the multi_channel_limit test

--- testing/poi_scan_timing/run.sh
+++ testing/poi_scan_timing/run.sh
@@ -1,3 +1,4 @@
 poi_scan.py \
    -m examples/multi_channel.json \
-   -y xs_BSM=0:2.5:20000 -o poi_scan_timing -t --batch-mode
+   -d examples/multi_channel_scan_data.json \
+   -y xs_BSM=0:2.5:20000 -o poi_scan_timing --show-timing --batch-mode
