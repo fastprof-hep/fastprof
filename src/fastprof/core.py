6a5e4474165ee2de938badde87b3f172342f57f9
Nicolas Berger <nicolas.berger@cern.ch>
2022-01-15 01:33:18 +0100

    comment improvement

--- src/fastprof/core.py
+++ src/fastprof/core.py
@@ -1055,7 +1055,7 @@ class Model (Serializable) :
       if channel.name in self.channels :
         raise ValueError('ERROR: multiple channels defined with the same name (%s)' % channel.name)
       for sample in channel.samples.values() :
-        # nominal yields can be None, which corresponds to [1...1] so norm carries all the information
+        # nominal yields can be None, which corresponds to [nominal_norm]*nbins which automatically works
         if sample.nominal_yields is not None and len(sample.nominal_yields) != channel.nbins() :
           raise ValueError("ERROR: sample '%s' of channel '%s' has nominal_yields of the wrong size (%d, expected %d)."
                            % (sample.name, channel.name, len(sample.nominal_yields), channel.nbins()))
