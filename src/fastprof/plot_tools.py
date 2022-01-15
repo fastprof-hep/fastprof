2f86f3e492909dfde2ca199af685f9934c34db46
Nicolas Berger <nicolas.berger@cern.ch>
2022-01-15 01:03:54 +0100

    add utility to plot best-fit values

--- src/fastprof/plot_tools.py
+++ src/fastprof/plot_tools.py
@@ -42,9 +42,10 @@ class PlotResults :
     self.pars = pars
     self.data = data
 
-  def decorate_matrix(self) :
-    plt.xticks(np.arange(len(self.data.model.pois)), self.data.model.pois.keys(), rotation='vertical')
-    plt.yticks(np.arange(len(self.data.model.pois)), self.data.model.pois.keys())
+  def decorate_with_pois(self, axes: str = 'xy', reverse: bool = False) :
+    indices = np.arange(len(self.data.model.pois)) if not reverse else np.arange(len(self.data.model.pois), 0, -1)
+    if 'x' in axes : plt.xticks(indices, self.data.model.pois.keys(), rotation='vertical')
+    if 'y' in axes : plt.yticks(indices, self.data.model.pois.keys())
 
   def make_canvas(self, figsize : tuple = (10,10), fig : plt.Figure = None) :
     if not fig :
@@ -53,15 +54,37 @@ class PlotResults :
       plt.figure(canvas.number)
 
   def plot_correlation(self, figsize : tuple = (10,10), fig : plt.Figure = None) :
-    self.make_canvas()
+    self.make_canvas(figsize, fig)
     img = plt.imshow(self.data.model.correlation_matrix(self.pars, self.data), vmin=-1, vmax=1, cmap='seismic')
-    self.decorate_matrix()
+    self.decorate_with_pois(axes='xy')
     plt.colorbar(img)
     plt.tight_layout()
 
   def plot_covariance(self, figsize : tuple = (10,10), fig : plt.Figure = None) :
-    self.make_canvas()
+    self.make_canvas(figsize, fig)
     img = plt.imshow(self.data.model.covariance_matrix(self.pars, self.data), cmap='seismic')
-    self.decorate_matrix()
+    self.decorate_with_pois(axes='xy')
     plt.colorbar(img)
+    plt.tight_layout()
+    
+  def plot_best_fit(self, pars: Parameters = None, values: dict = None, sym_errors : dict = None, results : dict = None, figsize : tuple = (10,10), fig : plt.Figure = None) :
+    self.make_canvas(figsize, fig)
+    vals = np.zeros(len(self.data.model.pois))
+    if pars is not None :
+      vals = np.array([ pars[poi] for poi in self.data.model.pois ])
+    elif values is not None :
+      vals = np.array([ values[poi] for poi in self.data.model.pois ])
+    elif results is not None :
+      vals = np.array([ results[poi][0] for poi in self.data.model.pois ])
+    else :
+      raise ValueError("Parameter values should be provided either through the 'pars', 'values' or 'results' arguments.")
+    if sym_errors is not None :
+      sym_errs = [ sym_errors[poi] for poi in self.data.model.pois ]
+      errs = np.array([ sym_errs, sym_errs ])
+    elif results is not None :
+      errs = np.array([[ results[poi][1] for poi in self.data.model.pois ], [ results[poi][2] for poi in self.data.model.pois ]])
+    else :
+      errs = np.zeros((2,len(self.data.model.pois)))
+    plt.errorbar(vals, np.arange(len(self.data.model.pois), 0, -1), yerr=errs, fmt='o')
+    self.decorate_with_pois(axes='y', reverse=True)
     plt.tight_layout()
