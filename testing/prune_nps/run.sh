ef26e24a7a020c020789b96fa1494f9c034ea7bb
Nicolas Berger <nicolas.berger@cern.ch>
2023-01-17 21:06:41 +0100

    update tests

--- testing/prune_nps/run.sh
+++ testing/prune_nps/run.sh
@@ -1,6 +1,8 @@
 prune_model.py -m examples/high_stats_two_bins.json \
                -o high_stats_two_bins_lessNPs.json \
                -p np_bkg1=52,np_eff=-1
+sed -e 's/^/@ /' high_stats_two_bins_lessNPs.json
+echo
 poi_scan.py -m high_stats_two_bins_lessNPs.json \
             -d examples/high_stats_two_bins.json \
             -y xs1=0:20:20 -o test_prune
--- testing/reparam/run.sh
+++ testing/reparam/run.sh
@@ -5,5 +5,6 @@ reparam_model.py -m examples/high_stats_two_bins.json \
    -o high_stats_two_bins_xx1.json
 #poi_scan.py -m examples/high_stats_two_bins.json -y xs1=0:20:20 -o reparam_xs1 -x
 sed -e 's/^/@ /' high_stats_two_bins_xx1.json
+echo
 poi_scan.py -m high_stats_two_bins_xx1.json -d examples/high_stats_two_bins.json -y xx1=0:20:20 -o reparam_xx1 -x
 
