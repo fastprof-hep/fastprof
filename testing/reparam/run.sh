d62819a99c22f09870583e9610b9d8718edc9ecc
Nicolas Berger <nicolas.berger@cern.ch>
2023-01-17 17:39:55 +0100

    add one more test for reparameterization

---
+++ testing/reparam/run.sh
@@ -0,0 +1,9 @@
+reparam_model.py -m examples/high_stats_two_bins.json \
+   -e "totalXX:linear_combination:xx1*2#xs2*1" \
+   -n SR1:Signal:totalXX \
+   -a xx1=0:0:20 -r xs1 \
+   -o high_stats_two_bins_xx1.json
+#poi_scan.py -m examples/high_stats_two_bins.json -y xs1=0:20:20 -o reparam_xs1 -x
+sed -e 's/^/@ /' high_stats_two_bins_xx1.json
+poi_scan.py -m high_stats_two_bins_xx1.json -d examples/high_stats_two_bins.json -y xx1=0:20:20 -o reparam_xx1 -x
+
