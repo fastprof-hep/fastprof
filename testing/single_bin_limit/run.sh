05d2556c0b2e655f36e39243d873fe8eb27eb6c7
Nicolas Berger <nicolas.berger@cern.ch>
2024-09-20 21:10:08 +0200

    add one point to get the full 1sigma asymptotic band

--- testing/single_bin_limit/run.sh
+++ testing/single_bin_limit/run.sh
@@ -1,4 +1,4 @@
 compute_fast_limits.py \
    -m doc/tutorials/counting_limit/inputs/single_bin.json \
-   -y "xs_BSM=1|xs_BSM=3|xs_BSM=5|xs_BSM=7|xs_BSM=9|xs_BSM=12" \
+   -y "xs_BSM=1|xs_BSM=3|xs_BSM=5|xs_BSM=7|xs_BSM=9|xs_BSM=12|xs_BSM=15" \
    -n 500 -s 131071 -o single_bin_limit -b 2
