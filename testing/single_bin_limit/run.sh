bdc52f5dba441d8b4fd18038d37d74b37dc92824
Nicolas Berger <nicolas.berger@cern.ch>
2024-09-21 21:37:00 +0200

    update reference and tweak hypos to catch all the bands

--- testing/single_bin_limit/run.sh
+++ testing/single_bin_limit/run.sh
@@ -1,4 +1,5 @@
 compute_fast_limits.py \
    -m doc/tutorials/counting_limit/inputs/single_bin.json \
-   -y "xs_BSM=1|xs_BSM=3|xs_BSM=5|xs_BSM=7|xs_BSM=9|xs_BSM=12|xs_BSM=15" \
-   -n 500 -s 131071 -o single_bin_limit -b 2
+   -y "xs_BSM=1|xs_BSM=3|xs_BSM=5|xs_BSM=7|xs_BSM=9|xs_BSM=15|xs_BSM=25" \
+   -n 500 -s 131071 -o single_bin_limit -b 2 --clsb
+
